c4eff866b3fc47cffab18a2658689a1495bf3bd2
s_stalwart_s <dev.soheil.ss@gmail.com>
2021-10-18 01:28:43 +0330

    Module-alias configured

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
+require("module-alias/register");
+// const test = require("./Scripts/Functions/temp");
+
 const express = require("express");
+const test = require("~/temp");
 const app = express();
 
 app.listen(3000, () => {
 	console.log("Server initialized");
 });
+
+test();

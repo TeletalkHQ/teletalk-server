c98cac31b8aec0ccb3bb457f808d878403bfbeff
s_stalwart_s <dev.soheil.ss@gmail.com>
2021-10-24 00:49:09 +0330

    Some initial

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+exports.connectDB = async () => {
+	try {
+		const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+			useFindAndModify: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		console.log(`MongoDB connected: ${connection.host}`);
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+	}
+};

6b444670f7a59a7129a0e28f51694ad93bcf04f0
S-STALWART-S <stalwart.programmer@gmail.com>
2021-11-05 13:29:31 +0330

    some utility added

---
+++ src/function/utility/bodyClearify.js
@@ -0,0 +1,13 @@
+const bodyClarify = (body) => {
+	const filteredBody = {};
+
+	Object.entries(body).forEach(([key, value]) => {
+		if (value) {
+			filteredBody[key] = value;
+		}
+	});
+
+	return { body: filteredBody };
+};
+
+module.exports = bodyClarify;
--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ app.listen(PORT, serverListenerCB);
 // 	);
 // 	res.header(
 // 		"Access-Control-Allow-Headers",
-// 		"Origin, X-Requested-With, Content-Type, Accept, Userorization"
+// 		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
 // 	);
 // 	next();
 // });

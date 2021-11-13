502a44b314cdd67689c1dbc455383029a2371d1c
S-STALWART-S <stalwart.programmer@gmail.com>
2021-11-14 01:26:18 +0330

    added: favicon route , helmet

--- src/middleware/errorResponser.js
+++ src/middleware/errorResponser.js
@@ -1,21 +1,25 @@
 const { myConsole } = require("~/function/utility/myConsole");
 
-const errorResponser = (req, res, next) => {
-	const {
-		categorizedLength,
-		uncategorizedLength,
-		categorized,
-		uncategorized,
-		statusCode,
-	} = res.errors;
-	if (categorizedLength || uncategorizedLength) {
-		myConsole.redBright(statusCode, "red").log();
-		res
-			.status(statusCode || 400)
-			.json({ errors: { categorized, uncategorized, statusCode } });
-	} else {
-		console.log("errorResponser else");
-		next();
+const errorResponser = ({ req, res, next, statusCode }) => {
+	try {
+		const {
+			categorizedLength,
+			uncategorizedLength,
+			categorized,
+			uncategorized,
+			statusCode: statusCodeFromCollector,
+		} = res.errors;
+		if (categorizedLength || uncategorizedLength) {
+			myConsole.redBright(statusCode, "red").log();
+
+			const code = statusCode || statusCodeFromCollector || 400;
+
+			res.status(code).json({ errors: { categorized, uncategorized, code } });
+		} else {
+			next();
+		}
+	} catch (error) {
+		myConsole.redBright("BAD ERROR!!!").log();
 	}
 };
 

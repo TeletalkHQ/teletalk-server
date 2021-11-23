b729caa299c7517e5777c65d01e7d246ee784c39
S-STALWART-S <stalwart.programmer@gmail.com>
2021-11-23 22:52:02 +0330

    Big changes: names, locations, and more

---
+++ src/controller/cellphoneController/removeBlockCellphoneController.js
@@ -0,0 +1,9 @@
+const removeBlockCellphoneController = (req, res) => {
+	try {
+	} catch (error) {
+		res.errorCollector({ error });
+		res.errorResponser();
+	}
+};
+
+module.exports = { removeBlockCellphoneController };

bd92f9cc9bea3481622db3e1fde232575a7b7d56
STALWART <dev.soheil.ss@gmail.com>
2021-11-26 16:50:37 +0330

    way on make messages work

---
+++ src/constant/error/messageError/messageError.js
@@ -0,0 +1,19 @@
+const messageError = {
+	CHAT_ID_INVALID_TYPE: {
+		reason: "CHAT_ID_INVALID_TYPE",
+	},
+	CHAT_ID_EXIST: {
+		reason: "CHAT_ID_EXIST",
+	},
+	CHAT_ID_REQUIRED: {
+		reason: "CHAT_ID_REQUIRED",
+	},
+	CHAT_ID_MIN_LENGTH_REACH: {
+		reason: "CHAT_ID_MIN_LENGTH_REACH",
+	},
+	CHAT_ID_MAX_LENGTH_REACH: {
+		reason: "CHAT_ID_MAX_LENGTH_REACH",
+	},
+};
+
+module.exports = { messageError };

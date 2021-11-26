a6e94bfbfa1dbba12a14a09547759ec41d404fae
S-STALWART-S <stalwart.programmer@gmail.com>
2021-11-26 16:50:37 +0330

    way on make messages work

--- src/template/schemaTemplate/messageSchemaTemplate.js
+++ src/template/schemaTemplate/messageSchemaTemplate.js
@@ -1,5 +1,10 @@
 const {
 	messageError: {
+		CHAT_ID_INVALID_TYPE,
+		CHAT_ID_EXIST,
+		CHAT_ID_REQUIRED,
+		CHAT_ID_MIN_LENGTH_REACH,
+		CHAT_ID_MAX_LENGTH_REACH,
 		CREATED_AT_INVALID_TYPE,
 		MESSAGE_ID_INVALID_TYPE,
 		MESSAGE_ID_EXIST,
@@ -7,7 +12,7 @@ const {
 		MESSAGE_ID_MIN_LENGTH_REACH,
 		MESSAGE_ID_MAX_LENGTH_REACH,
 	},
-} = require("~/constant/error/userError/messageError");
+} = require("~/constant/error/messageError/messageError");
 
 const fn = (value, error = { reason: "undefined", message: "undefined" }) => ({
 	value,
@@ -15,21 +20,35 @@ const fn = (value, error = { reason: "undefined", message: "undefined" }) => ({
 });
 
 const messageSchemaTemplate = {
-	messageID: {
-		type: fn("string", MESSAGE_ID_INVALID_TYPE),
-		Type: fn(String, MESSAGE_ID_INVALID_TYPE),
-		unique: fn(true, MESSAGE_ID_EXIST),
-		required: fn(true, MESSAGE_ID_REQUIRED),
-		minlength: fn(30, MESSAGE_ID_MIN_LENGTH_REACH),
-		maxlength: fn(35, MESSAGE_ID_MAX_LENGTH_REACH),
+	chatID: {
+		type: fn("string", CHAT_ID_INVALID_TYPE),
+		Type: fn(String, CHAT_ID_INVALID_TYPE),
+		unique: fn(true, CHAT_ID_EXIST),
+		required: fn(true, CHAT_ID_REQUIRED),
+		minlength: fn(30, CHAT_ID_MIN_LENGTH_REACH),
+		maxlength: fn(35, CHAT_ID_MAX_LENGTH_REACH),
 		trim: fn(true, "undefined"),
 	},
-	createdAt: {
-		type: fn("date", CREATED_AT_INVALID_TYPE),
-		Type: fn(Date, CREATED_AT_INVALID_TYPE),
-		required: fn(true, "undefined"),
-		default: fn(Date.now, "undefined"),
-	},
+	messages: [
+		{
+			messageID: {
+				type: fn("string", MESSAGE_ID_INVALID_TYPE),
+				Type: fn(String, MESSAGE_ID_INVALID_TYPE),
+				unique: fn(true, MESSAGE_ID_EXIST),
+				required: fn(true, MESSAGE_ID_REQUIRED),
+				minlength: fn(30, MESSAGE_ID_MIN_LENGTH_REACH),
+				maxlength: fn(35, MESSAGE_ID_MAX_LENGTH_REACH),
+				trim: fn(true, "undefined"),
+			},
+			messageText: {},
+			createdAt: {
+				type: fn("date", CREATED_AT_INVALID_TYPE),
+				Type: fn(Date, CREATED_AT_INVALID_TYPE),
+				required: fn(true, "undefined"),
+				default: fn(Date.now, "undefined"),
+			},
+		},
+	],
 };
 
 module.exports = { messageSchemaTemplate };

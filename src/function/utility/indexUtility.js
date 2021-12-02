685fef178e3196e2f4c01081d8f8f83b35134007
S-STALWART-S <stalwart.programmer@gmail.com>
2021-12-02 09:27:16 +0330

    PrivateMessage model and schema added

--- src/function/utility/indexUtility.js
+++ src/function/utility/indexUtility.js
@@ -2,7 +2,7 @@ const { myConsole } = require("~/function/utility/myConsole");
 const { objectClarify } = require("~/function/utility/objectClarify");
 const { passwordGenerator } = require("~/function/utility/passwordGenerator");
 const { randomID } = require("~/function/utility/randomID");
-const { tokenSigner } = require(~/function/utility/tokenMakerr");
+const { tokenSigner } = require("~/function/utility/tokenSigner");
 
 module.exports = {
 	myConsole,

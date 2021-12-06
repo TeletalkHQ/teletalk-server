353dfca66ca75ff4b156abc9e7a8fa820f372d8d
s-stalwart-s <dev.soheil.ss@gmail.com>
2021-12-06 23:31:06 +0330

    server file cleanedup

---
+++ src/other/globals.js
@@ -0,0 +1,4 @@
+const { myConsole } = require("~/function/utility/myConsole");
+
+global.myConsole = myConsole;
+global.logger = myConsole;
--- src/server.js
+++ src/server.js
@@ -1,71 +1,38 @@
 //* No more need attention to make absolute path
+//! Require before anyone!
 require("module-alias/register");
 
-const path = require("path");
+require("~/other/globals");
+
 const express = require("express");
 
-const helmet = require("helmet");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
-const serveFavicon = require("serve-favicon");
-
-const prettyError = require("pretty-error");
 
 const { connectDB } = require("~/config/database/connectDB");
 
-const { myConsole } = require("./function/utility/myConsole");
-global.myConsole = myConsole;
-global.logger = myConsole;
+const { middleLine } = require("~/middleware/middleLine");
 
 const { lifeLine } = require("~/route/lifeLine");
 
-const { bodyClarify } = require("~/middleware/bodyClarify");
-
-const { errorCollector } = require("~/middleware/errorCollector");
-const { errorResponser } = require("~/middleware/errorResponser");
-
 dotenv.config({ path: "./src/config/environment/main.env" });
 
-//* Connect to database =>
+//? Connect to database =>
 connectDB();
 
 const app = express();
 
-app.use((req, res, next) => {
-	console.log("Console cleared");
-	next();
-});
-
-//* Pretty error makes nodeJS error pretty in console, use it before express call
-prettyError.start();
-
-app.use(helmet());
-app.use(morgan("dev"));
-app.use(express.json());
-
-app.use(bodyClarify);
-app.use(errorCollector);
-app.use(errorResponser);
-
-app.use((req, res, next) => {
-	logger.bgBlue("Request arrived").bgCyan(req.url);
-
-	next();
-});
+middleLine({ app, express });
 
 //* Your statics is here =>
-app.use(express.static(path.join(__dirname, "..", "public")));
-app.use(serveFavicon(path.join(__dirname, "..", "public", "app_favicon", "favicon.ico")));
+app.use(express.static("~/../public"));
 
-//* All stuff for response to routes is in lifeLine =>
+//* All stuff for routes is in lifeLine =>
 app.use(lifeLine);
 
-app.use(errorResponser);
-
-const { PORT, NODE_ENV: MODE } = process.env;
+const { PORT, NODE_ENV } = process.env;
 
 const serverListenerCB = () => {
-	console.log(`Server is running in ${MODE} mode on port ${PORT}`);
+	console.log(`Server is running in ${NODE_ENV} mode on port ${PORT}`);
 };
 
 //* Control your error here =>

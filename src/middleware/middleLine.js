c0412869c274a6168b1f4ab71a8fa4437976cf55
S-STALWART-S <stalwart.programmer@gmail.com>
2021-12-06 23:31:06 +0330

    server file cleanedup

---
+++ src/middleware/middleLine.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+const helmet = require("helmet");
+const morgan = require("morgan");
+const prettyError = require("pretty-error");
+const serveFavicon = require("serve-favicon");
+
+const { bodyClarify } = require("~/middleware/bodyClarify");
+const { errorCollector } = require("~/middleware/errorCollector");
+const { errorResponser } = require("~/middleware/errorResponser");
+
+const middleLine = ({ app, express }) => {
+	prettyError.start();
+
+	app.use((req, res, next) => {
+		console.log("Console cleared");
+		next();
+	});
+
+	//* Pretty error makes nodeJS error pretty in console, use it before express call
+
+	app.use(helmet());
+	app.use(morgan("dev"));
+	app.use(express.json());
+
+	app.use(bodyClarify);
+	app.use(errorCollector);
+	app.use(errorResponser);
+
+	app.use(serveFavicon(path.join("~/../public/appFavicon/favicon.ico")));
+
+	app.use((req, res, next) => {
+		logger.bgBlue("Request arrived").bgCyan(req.url);
+
+		next();
+	});
+};
+
+module.exports = { middleLine };

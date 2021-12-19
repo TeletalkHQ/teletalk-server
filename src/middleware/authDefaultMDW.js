8d3c13ca06a3d2da8661b74026ca3c65f463ba03
s-stalwart-s <dev.soheil.ss@gmail.com>
2021-12-19 22:03:15 +0330

    sign and verify fixed for multi sign

--- src/middleware/authDefaultMDW.js
+++ src/middleware/authDefaultMDW.js
@@ -4,11 +4,12 @@ const authDefaultMDW = async (req, res, next) => {
 	try {
 		const token = req.headers.authorization?.split("Bearer ")[1];
 
+		console.log("authDefaultMDW", token);
 		req.body.authData = await tokenVerifier({ token });
 
 		next();
 	} catch (error) {
-		// console.log("🚀 ~ file: authDefaultMDW.js ~ line 11 ~ authDefaultMDW ~ error", error);
+		console.log("🚀 ~ file: authDefaultMDW.js ~ line 11 ~ authDefaultMDW ~ error", error);
 		res.errorCollector({ data: { error, statusCode: 401 } });
 		res.errorResponser();
 	} finally {

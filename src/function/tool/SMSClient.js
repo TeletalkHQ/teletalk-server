7aa4f7cae726fc111347ca41a87fd05b39beecdc
s-stalwart-s <dev.soheil.ss@gmail.com>
2021-12-30 11:01:46 +0330

    remove SMSClient from local. exclude secrets(env's)

--- src/function/tool/SMSClient.js
+++ src/function/tool/SMSClient.js
@@ -1,22 +1,15 @@
 const MelipayamakApi = require("melipayamak");
-// const MelipayamakApi = require("./melipayamak");
-
-const { SMS_CLIENT_PASSWORD, SMS_CLIENT_USERNAME } = require("~/config/secret/secret");
 
 const USERNAME = process.env.SMS_CLIENT_USERNAME;
 const PASSWORD = process.env.SMS_CLIENT_PASSWORD;
 
 console.log("from env", USERNAME, PASSWORD);
-console.log("from secret", SMS_CLIENT_USERNAME, SMS_CLIENT_PASSWORD);
 
-const api = new MelipayamakApi(
-	USERNAME || SMS_CLIENT_USERNAME,
-	PASSWORD || SMS_CLIENT_PASSWORD,
-);
+const api = new MelipayamakApi(USERNAME, PASSWORD);
 
 const sms = api.sms();
 const from = "50004001700470";
-const to = "09129675269";
+const to = "09012700470";
 const text = "Hi! this sms is from teletalk! Your verify code is: 605854";
 
 const isFlash = false;

502f8411402e1c58ebf7a46b421902ccb0f9a6b5
S-STALWART-S <stalwart.programmer@gmail.com>
2021-12-30 10:13:24 +0330

    ...

--- src/function/tool/SMSClient.js
+++ src/function/tool/SMSClient.js
@@ -2,14 +2,16 @@ const MelipayamakApi = require("melipayamak");
 const { SMS_CLIENT_PASSWORD, SMS_CLIENT_USERNAME } = require("~/config/secret/secret");
 
 const api = new MelipayamakApi(
-	process.env.SMS_CLIENT_USERNAME,
-	process.env.SMS_CLIENT_PASSWORD,
+	// process.env.SMS_CLIENT_USERNAME |
+	SMS_CLIENT_USERNAME,
+	// process.env.SMS_CLIENT_PASSWORD |
+	SMS_CLIENT_PASSWORD,
 );
 
 const sms = api.sms();
 const from = "50004001700470";
-const to = "09012700470";
-const text = "تست وب سرویس ملی پیامک";
+const to = "09129675269";
+const text = "Hi! this sms is from teletalk!\n Your verify code is: 605854";
 
 const isFlash = false;
 

13d0a88142308cbce9607e71c1bb9f5882f5a835
s-stalwart-s <dev.soheil.ss@gmail.com>
2021-12-30 10:43:37 +0330

    test with sms client package

--- src/function/tool/SMSClient.js
+++ src/function/tool/SMSClient.js
@@ -1,16 +1,23 @@
-// const MelipayamakApi = require("melipayamak");
-const MelipayamakApi = require("./melipayamak");
+const MelipayamakApi = require("melipayamak");
+// const MelipayamakApi = require("./melipayamak");
 
 const { SMS_CLIENT_PASSWORD, SMS_CLIENT_USERNAME } = require("~/config/secret/secret");
+
+const USERNAME = process.env.SMS_CLIENT_USERNAME;
+const PASSWORD = process.env.SMS_CLIENT_PASSWORD;
+
+console.log("from env", USERNAME, PASSWORD);
+console.log("from secret", SMS_CLIENT_USERNAME, SMS_CLIENT_PASSWORD);
+
 const api = new MelipayamakApi(
-	process.env.SMS_CLIENT_USERNAME | SMS_CLIENT_USERNAME,
-	process.env.SMS_CLIENT_PASSWORD | SMS_CLIENT_PASSWORD,
+	USERNAME || SMS_CLIENT_USERNAME,
+	PASSWORD || SMS_CLIENT_PASSWORD,
 );
 
 const sms = api.sms();
 const from = "50004001700470";
 const to = "09129675269";
-const text = "Hi! this sms is from teletalk!\n Your verify code is: 605854";
+const text = "Hi! this sms is from teletalk! Your verify code is: 605854";
 
 const isFlash = false;
 

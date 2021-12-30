9b64f9f8e6c96955a4d10f1ed850524b65a43736
S-STALWART-S <stalwart.programmer@gmail.com>
2021-12-30 10:33:20 +0330

    WHY?!?!?!?!?

--- src/function/tool/SMSClient.js
+++ src/function/tool/SMSClient.js
@@ -1,25 +1,27 @@
 // const MelipayamakApi = require("melipayamak");
-const { SMS_CLIENT_PASSWORD, SMS_CLIENT_USERNAME } = require("~/config/secret/secret");
-const MelipayamakApi = require("./melipayamak");
+// const MelipayamakApi = require("./melipayamak");
 
-const api = new MelipayamakApi(
-	// process.env.SMS_CLIENT_USERNAME |
-	SMS_CLIENT_USERNAME,
-	// process.env.SMS_CLIENT_PASSWORD |
-	SMS_CLIENT_PASSWORD,
-);
+// const { SMS_CLIENT_PASSWORD, SMS_CLIENT_USERNAME } = require("~/config/secret/secret");
+// const api = new MelipayamakApi(
+// 	// process.env.SMS_CLIENT_USERNAME |
+// 	SMS_CLIENT_USERNAME,
+// 	// process.env.SMS_CLIENT_PASSWORD |
+// 	SMS_CLIENT_PASSWORD,
+// );
 
-const sms = api.sms();
-const from = "50004001700470";
-const to = "09129675269";
-const text = "Hi! this sms is from teletalk!\n Your verify code is: 605854";
+// const sms = api.sms();
+// const from = "50004001700470";
+// const to = "09129675269";
+// const text = "Hi! this sms is from teletalk!\n Your verify code is: 605854";
 
-const isFlash = false;
+// const isFlash = false;
 
-const SMSClient = async () => {
-	const result = await sms.send(to, from, text, isFlash);
+// const SMSClient = async () => {
+// 	const result = await sms.send(to, from, text, isFlash);
 
-	console.log(result);
-};
+// 	console.log(result);
+// };
+
+const SMSClient = () => {};
 
 module.exports = { SMSClient };

1e60113bcb8f796d5f310997e9e23d9779ccd2ed
S-STALWART-S <stalwart.programmer@gmail.com>
2022-02-28 01:32:05 +0330

    schema templates cleanup

--- src/template/schemaTemplate/chatSchemaTemplate.js
+++ src/template/schemaTemplate/chatSchemaTemplate.js
@@ -1,132 +1,98 @@
 const { randomID } = require("~/function/utility/randomID");
+const {
+	schemaPropertyKeyGenerator,
+	schemaTemplateGenerator,
+} = require("~/function/utility/generators");
 
 const {
-	userSchemaTemplate: {
-		privateID: { properties: privateID },
-	},
+	userSchemaTemplate: { privateID },
 } = require("~/template/schemaTemplate/userSchemaTemplate");
-
 const { commonSchemaTemplate } = require("~/template/schemaTemplate/commonSchemaTemplate");
-
 const {
 	chatErrorTemplate: {
-		CHAT_ID_EXIST: { properties: CHAT_ID_EXIST },
-		CHAT_ID_INVALID_TYPE: { properties: CHAT_ID_INVALID_TYPE },
-		CHAT_ID_MAX_LENGTH_REACH: { properties: CHAT_ID_MAX_LENGTH_REACH },
-		CHAT_ID_MIN_LENGTH_REACH: { properties: CHAT_ID_MIN_LENGTH_REACH },
-		CHAT_ID_REQUIRED: { properties: CHAT_ID_REQUIRED },
-		MESSAGE_ID_EXIST: { properties: MESSAGE_ID_EXIST },
-		MESSAGE_ID_INVALID_TYPE: { properties: MESSAGE_ID_INVALID_TYPE },
-		MESSAGE_ID_MAX_LENGTH_REACH: { properties: MESSAGE_ID_MAX_LENGTH_REACH },
-		MESSAGE_ID_MIN_LENGTH_REACH: { properties: MESSAGE_ID_MIN_LENGTH_REACH },
-		MESSAGE_ID_REQUIRED: { properties: MESSAGE_ID_REQUIRED },
-		MESSAGE_TEXT_INVALID_TYPE: { properties: MESSAGE_TEXT_INVALID_TYPE },
-		MESSAGE_TEXT_MAX_LENGTH_REACH: { properties: MESSAGE_TEXT_MAX_LENGTH_REACH },
-		MESSAGE_TEXT_MIN_LENGTH_REACH: { properties: MESSAGE_TEXT_MIN_LENGTH_REACH },
-		PARTICIPANT_ID_EXIST: { properties: PARTICIPANT_ID_EXIST },
-		PARTICIPANT_ID_INVALID_TYPE: { properties: PARTICIPANT_ID_INVALID_TYPE },
-		PARTICIPANT_ID_MAX_LENGTH_REACH: { properties: PARTICIPANT_ID_MAX_LENGTH_REACH },
-		PARTICIPANT_ID_MIN_LENGTH_REACH: { properties: PARTICIPANT_ID_MIN_LENGTH_REACH },
-		PARTICIPANT_ID_REQUIRED: { properties: PARTICIPANT_ID_REQUIRED },
+		CHAT_ID_EXIST,
+		CHAT_ID_INVALID_TYPE,
+		CHAT_ID_MAX_LENGTH_REACH,
+		CHAT_ID_MIN_LENGTH_REACH,
+		CHAT_ID_REQUIRED,
+		MESSAGE_ID_EXIST,
+		MESSAGE_ID_INVALID_TYPE,
+		MESSAGE_ID_MAX_LENGTH_REACH,
+		MESSAGE_ID_MIN_LENGTH_REACH,
+		MESSAGE_ID_REQUIRED,
+		MESSAGE_TEXT_INVALID_TYPE,
+		MESSAGE_TEXT_MAX_LENGTH_REACH,
+		MESSAGE_TEXT_MIN_LENGTH_REACH,
+		PARTICIPANT_ID_EXIST,
+		PARTICIPANT_ID_INVALID_TYPE,
+		PARTICIPANT_ID_MAX_LENGTH_REACH,
+		PARTICIPANT_ID_MIN_LENGTH_REACH,
+		PARTICIPANT_ID_REQUIRED,
 	},
 } = require("~/template/errorTemplate/chatErrorTemplate");
 
-const fn = (value, error = { reason: "undefined", message: "undefined" }) => ({
-	value,
-	error,
-});
-
-const chatID = {
-	properties: {
-		maxlength: fn(35, CHAT_ID_MAX_LENGTH_REACH),
-		minlength: fn(30, CHAT_ID_MIN_LENGTH_REACH),
-		required: fn(true, CHAT_ID_REQUIRED),
-		trim: fn(true, "undefined"),
-		type: fn("string", CHAT_ID_INVALID_TYPE),
-		unique: fn(true, CHAT_ID_EXIST),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const chatID = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(35, CHAT_ID_MAX_LENGTH_REACH),
+	schemaPropertyKeyGenerator(30, CHAT_ID_MIN_LENGTH_REACH),
+	schemaPropertyKeyGenerator(true, CHAT_ID_REQUIRED),
+	schemaPropertyKeyGenerator(true),
+	schemaPropertyKeyGenerator("string", CHAT_ID_INVALID_TYPE),
+	schemaPropertyKeyGenerator(true, CHAT_ID_EXIST),
+	"1.0.0",
+);
 
 const createdAt = commonSchemaTemplate.createdAt;
 
-const messageID = {
-	properties: {
-		default: fn(randomID, "undefined"),
-		maxlength: fn(45, MESSAGE_ID_MAX_LENGTH_REACH),
-		minlength: fn(40, MESSAGE_ID_MIN_LENGTH_REACH),
-		required: fn(true, MESSAGE_ID_REQUIRED),
-		trim: fn(true, "undefined"),
-		type: fn("string", MESSAGE_ID_INVALID_TYPE),
-		unique: fn(true, MESSAGE_ID_EXIST),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const messageID = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(45, MESSAGE_ID_MAX_LENGTH_REACH),
+	schemaPropertyKeyGenerator(40, MESSAGE_ID_MIN_LENGTH_REACH),
+	schemaPropertyKeyGenerator(true, MESSAGE_ID_REQUIRED),
+	schemaPropertyKeyGenerator(true),
+	schemaPropertyKeyGenerator("string", MESSAGE_ID_INVALID_TYPE),
+	schemaPropertyKeyGenerator(true, MESSAGE_ID_EXIST),
+	schemaPropertyKeyGenerator(randomID),
+	"1.0.0",
+);
 
-const messageSender = {
-	info: {
-		version: "1.0.0",
-	},
-};
+const messageSender = schemaTemplateGenerator("1.0.0");
 
-const messageStatus = {
-	info: {
-		version: "1.0.0",
-	},
-};
+const messageStatus = schemaTemplateGenerator("1.0.0");
 
-const message = {
-	properties: {
-		maxlength: fn(10, MESSAGE_TEXT_MAX_LENGTH_REACH),
-		minlength: fn(1, MESSAGE_TEXT_MIN_LENGTH_REACH),
-		type: fn("string", MESSAGE_TEXT_INVALID_TYPE),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const message = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(10, MESSAGE_TEXT_MAX_LENGTH_REACH),
+	schemaPropertyKeyGenerator(1, MESSAGE_TEXT_MIN_LENGTH_REACH),
+	null,
+	null,
+	schemaPropertyKeyGenerator("string", MESSAGE_TEXT_INVALID_TYPE),
+	null,
+	null,
+	"1.0.0",
+);
 
-const participantID = {
-	properties: {
-		maxlength: fn(privateID.maxlength.value, PARTICIPANT_ID_MAX_LENGTH_REACH),
-		minlength: fn(privateID.minlength.value, PARTICIPANT_ID_MIN_LENGTH_REACH),
-		required: fn(privateID.required.value, PARTICIPANT_ID_REQUIRED),
-		trim: fn(privateID.trim.value, "undefined"),
-		type: fn(privateID.type.value, PARTICIPANT_ID_INVALID_TYPE),
-		unique: fn(privateID.unique.value, PARTICIPANT_ID_EXIST),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const participantID = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(privateID.maxlength.value, PARTICIPANT_ID_MAX_LENGTH_REACH),
+	schemaPropertyKeyGenerator(privateID.minlength.value, PARTICIPANT_ID_MIN_LENGTH_REACH),
+	schemaPropertyKeyGenerator(privateID.required.value, PARTICIPANT_ID_REQUIRED),
+	schemaPropertyKeyGenerator(privateID.trim.value),
+	schemaPropertyKeyGenerator(privateID.type.value, PARTICIPANT_ID_INVALID_TYPE),
+	schemaPropertyKeyGenerator(privateID.unique.value, PARTICIPANT_ID_EXIST),
+	null,
+	"1.0.0",
+);
 
-const participantStatus = {
-	info: {
-		version: "1.0.0",
-	},
-};
+const participantStatus = schemaTemplateGenerator("1.0.0");
 
-const participantVisibility = {
-	info: {
-		version: "1.0.0",
-	},
-};
+const participantVisibility = schemaTemplateGenerator("1.0.0");
 
 const chatSchemaTemplate = {
-	info: {
-		version: "1.0.0",
-	},
+	version: "1.0.0",
 
 	chatID,
 	createdAt,
+	message,
 	messageID,
 	messageSender,
 	messageStatus,
-	message,
 	participantID,
 	participantStatus,
 	participantVisibility,
@@ -134,14 +100,4 @@ const chatSchemaTemplate = {
 
 module.exports = {
 	chatSchemaTemplate,
-
-	chatID,
-	createdAt,
-	messageID,
-	messageSender,
-	messageStatus,
-	message,
-	participantID,
-	participantStatus,
-	participantVisibility,
 };
--- src/template/schemaTemplate/userSchemaTemplate.js
+++ src/template/schemaTemplate/userSchemaTemplate.js
@@ -1,231 +1,209 @@
-//* Using in mongoose schema and validators
+const {
+	schemaPropertyKeyGenerator,
+	schemaTemplateGenerator,
+} = require("~/function/utility/generators");
 
 const { commonSchemaTemplate } = require("~/template/schemaTemplate/commonSchemaTemplate");
 
 const {
 	userErrorTemplate: {
-		BIO_INVALID_TYPE: { properties: BIO_INVALID_TYPE },
-		BIO_MAXLENGTH_REACH: { properties: BIO_MAXLENGTH_REACH },
-		BIO_MINLENGTH_REACH: { properties: BIO_MINLENGTH_REACH },
-		BLACKLIST_INVALID_TYPE: { properties: BLACKLIST_INVALID_TYPE },
-		CONTACT_INVALID_TYPE: { properties: CONTACT_INVALID_TYPE },
-		COUNTRY_CODE_INVALID_TYPE: { properties: COUNTRY_CODE_INVALID_TYPE },
-		COUNTRY_CODE_MAXLENGTH_REACH: { properties: COUNTRY_CODE_MAXLENGTH_REACH },
-		COUNTRY_CODE_MINLENGTH_REACH: { properties: COUNTRY_CODE_MINLENGTH_REACH },
-		COUNTRY_CODE_REQUIRED: { properties: COUNTRY_CODE_REQUIRED },
-		COUNTRY_NAME_INVALID_TYPE: { properties: COUNTRY_NAME_INVALID_TYPE },
-		COUNTRY_NAME_MAXLENGTH_REACH: { properties: COUNTRY_NAME_MAXLENGTH_REACH },
-		COUNTRY_NAME_MINLENGTH_REACH: { properties: COUNTRY_NAME_MINLENGTH_REACH },
-		COUNTRY_NAME_REQUIRED: { properties: COUNTRY_NAME_REQUIRED },
-		FIRST_NAME_INVALID_TYPE: { properties: FIRST_NAME_INVALID_TYPE },
-		FIRST_NAME_MAXLENGTH_REACH: { properties: FIRST_NAME_MAXLENGTH_REACH },
-		FIRST_NAME_MINLENGTH_REACH: { properties: FIRST_NAME_MINLENGTH_REACH },
-		FIRST_NAME_REQUIRED: { properties: FIRST_NAME_REQUIRED },
-		LAST_NAME_INVALID_TYPE: { properties: LAST_NAME_INVALID_TYPE },
-		LAST_NAME_MAXLENGTH_REACH: { properties: LAST_NAME_MAXLENGTH_REACH },
-		LAST_NAME_MINLENGTH_REACH: { properties: LAST_NAME_MINLENGTH_REACH },
-		MAC_ADDRESS_EXIST: { properties: MAC_ADDRESS_EXIST },
-		MAC_ADDRESS_INVALID_TYPE: { properties: MAC_ADDRESS_INVALID_TYPE },
-		MAC_ADDRESS_MAXLENGTH_REACH: { properties: MAC_ADDRESS_MAXLENGTH_REACH },
-		MAC_ADDRESS_MINLENGTH_REACH: { properties: MAC_ADDRESS_MINLENGTH_REACH },
-		MAC_ADDRESS_REQUIRED: { properties: MAC_ADDRESS_REQUIRED },
-		PHONE_NUMBER_EXIST: { properties: PHONE_NUMBER_EXIST },
-		PHONE_NUMBER_INVALID_TYPE: { properties: PHONE_NUMBER_INVALID_TYPE },
-		PHONE_NUMBER_MAXLENGTH_REACH: { properties: PHONE_NUMBER_MAXLENGTH_REACH },
-		PHONE_NUMBER_MINLENGTH_REACH: { properties: PHONE_NUMBER_MINLENGTH_REACH },
-		PHONE_NUMBER_REQUIRED: { properties: PHONE_NUMBER_REQUIRED },
-		PRIVATE_ID_EXIST: { properties: PRIVATE_ID_EXIST },
-		PRIVATE_ID_INVALID_TYPE: { properties: PRIVATE_ID_INVALID_TYPE },
-		PRIVATE_ID_MAX_LENGTH_REACH: { properties: PRIVATE_ID_MAX_LENGTH_REACH },
-		PRIVATE_ID_MIN_LENGTH_REACH: { properties: PRIVATE_ID_MIN_LENGTH_REACH },
-		PRIVATE_ID_REQUIRED: { properties: PRIVATE_ID_REQUIRED },
-		TOKEN_EXIST: { properties: TOKEN_EXIST },
-		TOKEN_INVALID_TYPE: { properties: TOKEN_INVALID_TYPE },
-		TOKEN_REQUIRED: { properties: TOKEN_REQUIRED },
-		USERNAME_EXIST: { properties: USERNAME_EXIST },
-		USERNAME_INVALID_TYPE: { properties: USERNAME_INVALID_TYPE },
-		USERNAME_MAXLENGTH_REACH: { properties: USERNAME_MAXLENGTH_REACH },
-		USERNAME_MINLENGTH_REACH: { properties: USERNAME_MINLENGTH_REACH },
-		VERIFICATION_CODE_INVALID_LENGTH: { properties: VERIFICATION_CODE_INVALID_LENGTH },
-		VERIFICATION_CODE_INVALID_TYPE: { properties: VERIFICATION_CODE_INVALID_TYPE },
+		BIO_INVALID_TYPE,
+		BIO_MAXLENGTH_REACH,
+		BIO_MINLENGTH_REACH,
+		BLACKLIST_INVALID_TYPE,
+		CONTACT_INVALID_TYPE,
+		COUNTRY_CODE_INVALID_TYPE,
+		COUNTRY_CODE_MAXLENGTH_REACH,
+		COUNTRY_CODE_MINLENGTH_REACH,
+		COUNTRY_CODE_REQUIRED,
+		COUNTRY_NAME_INVALID_TYPE,
+		COUNTRY_NAME_MAXLENGTH_REACH,
+		COUNTRY_NAME_MINLENGTH_REACH,
+		COUNTRY_NAME_REQUIRED,
+		FIRST_NAME_INVALID_TYPE,
+		FIRST_NAME_MAXLENGTH_REACH,
+		FIRST_NAME_MINLENGTH_REACH,
+		FIRST_NAME_REQUIRED,
+		LAST_NAME_INVALID_TYPE,
+		LAST_NAME_MAXLENGTH_REACH,
+		LAST_NAME_MINLENGTH_REACH,
+		MAC_ADDRESS_EXIST,
+		MAC_ADDRESS_INVALID_TYPE,
+		MAC_ADDRESS_MAXLENGTH_REACH,
+		MAC_ADDRESS_MINLENGTH_REACH,
+		MAC_ADDRESS_REQUIRED,
+		PHONE_NUMBER_EXIST,
+		PHONE_NUMBER_INVALID_TYPE,
+		PHONE_NUMBER_MAXLENGTH_REACH,
+		PHONE_NUMBER_MINLENGTH_REACH,
+		PHONE_NUMBER_REQUIRED,
+		PRIVATE_ID_EXIST,
+		PRIVATE_ID_INVALID_TYPE,
+		PRIVATE_ID_MAX_LENGTH_REACH,
+		PRIVATE_ID_MIN_LENGTH_REACH,
+		PRIVATE_ID_REQUIRED,
+		TOKEN_EXIST,
+		TOKEN_INVALID_TYPE,
+		TOKEN_REQUIRED,
+		USERNAME_EXIST,
+		USERNAME_INVALID_TYPE,
+		USERNAME_MAXLENGTH_REACH,
+		USERNAME_MINLENGTH_REACH,
+		VERIFICATION_CODE_INVALID_LENGTH,
+		VERIFICATION_CODE_INVALID_TYPE,
 	},
 } = require("~/template/errorTemplate/userErrorTemplate");
 
-const fn = (value, error = { reason: "undefined", message: "undefined" }) => ({
-	value,
-	error,
-});
+const bio = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(255, BIO_MAXLENGTH_REACH),
+	schemaPropertyKeyGenerator(1, BIO_MINLENGTH_REACH),
+	schemaPropertyKeyGenerator(false),
+	null,
+	schemaPropertyKeyGenerator("string", BIO_INVALID_TYPE),
+	null,
+	schemaPropertyKeyGenerator(""),
+	"1.0.0",
+);
 
-const bio = {
-	properties: {
-		default: fn("", "undefined"),
-		maxlength: fn(255, BIO_MAXLENGTH_REACH),
-		minlength: fn(1, BIO_MINLENGTH_REACH),
-		required: fn(false, "undefined"),
-		type: fn("string", BIO_INVALID_TYPE),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const blacklist = schemaTemplateGenerator(
+	null,
+	null,
+	null,
+	null,
+	schemaPropertyKeyGenerator("array", BLACKLIST_INVALID_TYPE),
+	null,
+	schemaPropertyKeyGenerator([]),
+	"1.0.0",
+);
 
-const blacklist = {
-	properties: { default: fn([], "undefined"), type: fn("array", BLACKLIST_INVALID_TYPE) },
-	info: {
-		version: "1.0.0",
-	},
-};
+const contacts = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(14),
+	schemaPropertyKeyGenerator(10),
+	schemaPropertyKeyGenerator(false),
+	null,
+	schemaPropertyKeyGenerator("array", CONTACT_INVALID_TYPE),
+	null,
+	schemaPropertyKeyGenerator([]),
+	"1.0.0",
+);
 
-const contacts = {
-	properties: {
-		default: fn([], "undefined"),
-		maxlength: fn(14, "undefined"),
-		minlength: fn(10, "undefined"),
-		required: fn(false, "undefined"),
-		type: fn("array", CONTACT_INVALID_TYPE),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const countryCode = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(8, COUNTRY_CODE_MAXLENGTH_REACH),
+	schemaPropertyKeyGenerator(2, COUNTRY_CODE_MINLENGTH_REACH),
+	schemaPropertyKeyGenerator(true, COUNTRY_CODE_REQUIRED),
+	schemaPropertyKeyGenerator(true),
+	schemaPropertyKeyGenerator("string", COUNTRY_CODE_INVALID_TYPE),
+	null,
+	null,
+	"1.0.0",
+);
 
-const countryCode = {
-	properties: {
-		maxlength: fn(8, COUNTRY_CODE_MAXLENGTH_REACH),
-		minlength: fn(2, COUNTRY_CODE_MINLENGTH_REACH),
-		required: fn(true, COUNTRY_CODE_REQUIRED),
-		trim: fn(true, "undefined"),
-		type: fn("string", COUNTRY_CODE_INVALID_TYPE),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
-
-const countryName = {
-	properties: {
-		maxlength: fn(32, COUNTRY_NAME_MAXLENGTH_REACH),
-		minlength: fn(2, COUNTRY_NAME_MINLENGTH_REACH),
-		required: fn(true, COUNTRY_NAME_REQUIRED),
-		type: fn("string", COUNTRY_NAME_INVALID_TYPE),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const countryName = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(32, COUNTRY_NAME_MAXLENGTH_REACH),
+	schemaPropertyKeyGenerator(2, COUNTRY_NAME_MINLENGTH_REACH),
+	schemaPropertyKeyGenerator(true, COUNTRY_NAME_REQUIRED),
+	null,
+	schemaPropertyKeyGenerator("string", COUNTRY_NAME_INVALID_TYPE),
+	null,
+	null,
+	"1.0.0",
+);
 
 const createdAt = commonSchemaTemplate.createdAt;
 
-const firstName = {
-	properties: {
-		maxlength: fn(18, FIRST_NAME_MAXLENGTH_REACH),
-		minlength: fn(1, FIRST_NAME_MINLENGTH_REACH),
-		required: fn(true, FIRST_NAME_REQUIRED),
-		trim: fn(false, "undefined"),
-		type: fn("string", FIRST_NAME_INVALID_TYPE),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const firstName = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(18, FIRST_NAME_MAXLENGTH_REACH),
+	schemaPropertyKeyGenerator(1, FIRST_NAME_MINLENGTH_REACH),
+	schemaPropertyKeyGenerator(true, FIRST_NAME_REQUIRED),
+	schemaPropertyKeyGenerator(false),
+	schemaPropertyKeyGenerator("string", FIRST_NAME_INVALID_TYPE),
+	null,
+	null,
+	"1.0.0",
+);
 
-const lastName = {
-	properties: {
-		default: fn("", "undefined"),
-		maxlength: fn(18, LAST_NAME_MAXLENGTH_REACH),
-		minlength: fn(1, LAST_NAME_MINLENGTH_REACH),
-		required: [false],
-		trim: fn(false, "undefined"),
-		type: fn("string", LAST_NAME_INVALID_TYPE),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const lastName = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(18, LAST_NAME_MAXLENGTH_REACH),
+	schemaPropertyKeyGenerator(1, LAST_NAME_MINLENGTH_REACH),
+	[false],
+	schemaPropertyKeyGenerator(false),
+	schemaPropertyKeyGenerator("string", LAST_NAME_INVALID_TYPE),
+	null,
+	schemaPropertyKeyGenerator(""),
+	"1.0.0",
+);
 
-const macAddress = {
-	properties: {
-		maxlength: fn(16, MAC_ADDRESS_MAXLENGTH_REACH),
-		minlength: fn(12, MAC_ADDRESS_MINLENGTH_REACH),
-		required: fn(true, MAC_ADDRESS_REQUIRED),
-		trim: fn(true, "undefined"),
-		type: fn("string", MAC_ADDRESS_INVALID_TYPE),
-		unique: fn(true, MAC_ADDRESS_EXIST),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const macAddress = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(16, MAC_ADDRESS_MAXLENGTH_REACH),
+	schemaPropertyKeyGenerator(12, MAC_ADDRESS_MINLENGTH_REACH),
+	schemaPropertyKeyGenerator(true, MAC_ADDRESS_REQUIRED),
+	schemaPropertyKeyGenerator(true),
+	schemaPropertyKeyGenerator("string", MAC_ADDRESS_INVALID_TYPE),
+	schemaPropertyKeyGenerator(true, MAC_ADDRESS_EXIST),
+	null,
+	"1.0.0",
+);
 
-const phoneNumber = {
-	properties: {
-		maxlength: fn(14, PHONE_NUMBER_MAXLENGTH_REACH),
-		minlength: fn(10, PHONE_NUMBER_MINLENGTH_REACH),
-		required: fn(true, PHONE_NUMBER_REQUIRED),
-		type: fn("string", PHONE_NUMBER_INVALID_TYPE),
-		unique: fn(true, PHONE_NUMBER_EXIST),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const phoneNumber = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(14, PHONE_NUMBER_MAXLENGTH_REACH),
+	schemaPropertyKeyGenerator(10, PHONE_NUMBER_MINLENGTH_REACH),
+	schemaPropertyKeyGenerator(true, PHONE_NUMBER_REQUIRED),
+	null,
+	schemaPropertyKeyGenerator("string", PHONE_NUMBER_INVALID_TYPE),
+	schemaPropertyKeyGenerator(true, PHONE_NUMBER_EXIST),
+	null,
+	"1.0.0",
+);
 
-const privateID = {
-	properties: {
-		maxlength: fn(35, PRIVATE_ID_MAX_LENGTH_REACH),
-		minlength: fn(30, PRIVATE_ID_MIN_LENGTH_REACH),
-		required: fn(true, PRIVATE_ID_REQUIRED),
-		trim: fn(true, "undefined"),
-		type: fn("string", PRIVATE_ID_INVALID_TYPE),
-		unique: fn(true, PRIVATE_ID_EXIST),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const privateID = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(35, PRIVATE_ID_MAX_LENGTH_REACH),
+	schemaPropertyKeyGenerator(30, PRIVATE_ID_MIN_LENGTH_REACH),
+	schemaPropertyKeyGenerator(true, PRIVATE_ID_REQUIRED),
+	schemaPropertyKeyGenerator(true),
+	schemaPropertyKeyGenerator("string", PRIVATE_ID_INVALID_TYPE),
+	schemaPropertyKeyGenerator(true, PRIVATE_ID_EXIST),
+	null,
+	"1.0.0",
+);
 
-const token = {
-	properties: {
-		required: fn(true, TOKEN_REQUIRED),
-		type: fn("string", TOKEN_INVALID_TYPE),
-		unique: fn(true, TOKEN_EXIST),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const token = schemaTemplateGenerator(
+	null,
+	null,
+	schemaPropertyKeyGenerator(true, TOKEN_REQUIRED),
+	null,
+	schemaPropertyKeyGenerator("string", TOKEN_INVALID_TYPE),
+	schemaPropertyKeyGenerator(true, TOKEN_EXIST),
+	null,
+	"1.0.0",
+);
 
-const username = {
-	properties: {
-		default: fn("", "undefined"),
-		lowercase: fn(true, "undefined"),
-		maxlength: fn(12, USERNAME_MAXLENGTH_REACH),
-		minlength: fn(4, USERNAME_MINLENGTH_REACH),
-		required: fn(false, "undefined"),
-		trim: fn(true, "undefined"),
-		type: fn("string", USERNAME_INVALID_TYPE),
-		unique: fn(false, USERNAME_EXIST),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const username = schemaTemplateGenerator(
+	schemaPropertyKeyGenerator(12, USERNAME_MAXLENGTH_REACH),
+	schemaPropertyKeyGenerator(4, USERNAME_MINLENGTH_REACH),
+	schemaPropertyKeyGenerator(false),
+	schemaPropertyKeyGenerator(true),
+	schemaPropertyKeyGenerator("string", USERNAME_INVALID_TYPE),
+	schemaPropertyKeyGenerator(false, USERNAME_EXIST),
+	schemaPropertyKeyGenerator(""),
+	"1.0.0",
+	schemaPropertyKeyGenerator(true),
+);
 
-const verificationCode = {
-	properties: {
-		length: fn(6, VERIFICATION_CODE_INVALID_LENGTH),
-		trim: fn(true, "undefined"),
-		type: fn("string", VERIFICATION_CODE_INVALID_TYPE),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const verificationCode = schemaTemplateGenerator(
+	null,
+	null,
+	null,
+	schemaPropertyKeyGenerator(true),
+	schemaPropertyKeyGenerator("string", VERIFICATION_CODE_INVALID_TYPE),
+	null,
+	null,
+	"1.0.0",
+	null,
+	schemaPropertyKeyGenerator(6, VERIFICATION_CODE_INVALID_LENGTH),
+);
 
 const userSchemaTemplate = {
-	info: {
-		version: "1.0.0",
-	},
+	version: "1.0.0",
 
 	bio,
 	blacklist,
@@ -245,19 +223,4 @@ const userSchemaTemplate = {
 
 module.exports = {
 	userSchemaTemplate,
-
-	bio,
-	blacklist,
-	contacts,
-	countryCode,
-	countryName,
-	createdAt,
-	firstName,
-	lastName,
-	macAddress,
-	phoneNumber,
-	privateID,
-	token,
-	username,
-	verificationCode,
 };

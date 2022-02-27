0923b6e29b68e635b22b3ea2d7de5ebf2d6c1b79
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-02-28 01:32:05 +0330

    schema templates cleanup

--- src/template/schemaTemplate/commonSchemaTemplate.js
+++ src/template/schemaTemplate/commonSchemaTemplate.js
@@ -1,33 +1,29 @@
 const {
+	schemaPropertyKeyGenerator,
+	schemaTemplateGenerator,
+} = require("~/function/utility/generators");
+
+const {
 	userErrorTemplate: { CREATED_AT_INVALID_TYPE },
 } = require("~/template/errorTemplate/userErrorTemplate");
 
-const fn = (value, error = { reason: "undefined", message: "undefined" }) => ({
-	value,
-	error,
-});
-
-const createdAt = {
-	properties: {
-		default: fn(Date.now, "undefined"),
-		required: fn(true, "undefined"),
-		type: fn("date", CREATED_AT_INVALID_TYPE),
-	},
-	info: {
-		version: "1.0.0",
-	},
-};
+const createdAt = schemaTemplateGenerator(
+	null,
+	null,
+	schemaPropertyKeyGenerator(true),
+	null,
+	schemaPropertyKeyGenerator("date", CREATED_AT_INVALID_TYPE),
+	null,
+	schemaPropertyKeyGenerator(Date.now),
+	"1.0.0",
+);
 
 const commonSchemaTemplate = {
-	info: {
-		version: "1.0.0",
-	},
+	version: "1.0.0",
 
 	createdAt,
 };
 
 module.exports = {
 	commonSchemaTemplate,
-
-	createdAt,
 };

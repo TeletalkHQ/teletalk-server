ccffebb876e887ef868e1e4a7f48fa74871b410a
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-02-27 23:09:23 +0330

    error templates cleanup

--- src/template/errorTemplate/userErrorTemplate.js
+++ src/template/errorTemplate/userErrorTemplate.js
@@ -1,300 +1,319 @@
-const BLACKLIST_INVALID_TYPE = {
-	properties: { message: "", code: 4023, reason: "BLACKLIST_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const { errorTemplateGenerator } = require("~/function/utility/generators");
 
-const BIO_INVALID_TYPE = {
-	properties: { message: "", code: 4024, reason: "BIO_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const BLACKLIST_INVALID_TYPE = errorTemplateGenerator(
+	4023,
+	"",
+	"BLACKLIST_INVALID_TYPE",
+	"1.0.0",
+);
 
-const BIO_MAXLENGTH_REACH = {
-	properties: { message: "", code: 4025, reason: "BIO_MAXLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const BIO_INVALID_TYPE = errorTemplateGenerator(4024, "", "BIO_INVALID_TYPE", "1.0.0");
 
-const BIO_MINLENGTH_REACH = {
-	properties: { message: "", code: 4026, reason: "BIO_MINLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const BIO_MAXLENGTH_REACH = errorTemplateGenerator(4025, "", "BIO_MAXLENGTH_REACH", "1.0.0");
 
-const CELLPHONE_EXIST = {
-	properties: { message: "", code: 4027, reason: "CELLPHONE_EXIST" },
-	info: { version: "1.0.0" },
-};
+const BIO_MINLENGTH_REACH = errorTemplateGenerator(4026, "", "BIO_MINLENGTH_REACH", "1.0.0");
 
-const CELLPHONE_INVALID_TYPE = {
-	properties: { message: "", code: 4028, reason: "CELLPHONE_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const CELLPHONE_EXIST = errorTemplateGenerator(4027, "", "CELLPHONE_EXIST", "1.0.0");
 
-const CELLPHONE_MAXLENGTH_REACH = {
-	properties: { message: "", code: 4029, reason: "CELLPHONE_MAXLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const CELLPHONE_INVALID_TYPE = errorTemplateGenerator(
+	4028,
+	"",
+	"CELLPHONE_INVALID_TYPE",
+	"1.0.0",
+);
 
-const CELLPHONE_MINLENGTH_REACH = {
-	properties: { message: "", code: 4030, reason: "CELLPHONE_MINLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const CELLPHONE_MAXLENGTH_REACH = errorTemplateGenerator(
+	4029,
+	"",
+	"CELLPHONE_MAXLENGTH_REACH",
+	"1.0.0",
+);
 
-const CELLPHONE_NOT_EXIST = {
-	properties: { message: "", code: 4031, reason: "CELLPHONE_NOT_EXIST" },
-	info: { version: "1.0.0" },
-};
+const CELLPHONE_MINLENGTH_REACH = errorTemplateGenerator(
+	4030,
+	"",
+	"CELLPHONE_MINLENGTH_REACH",
+	"1.0.0",
+);
 
-const CELLPHONE_REQUIRED = {
-	properties: { message: "", code: 4032, reason: "CELLPHONE_REQUIRED" },
-	info: { version: "1.0.0" },
-};
+const CELLPHONE_NOT_EXIST = errorTemplateGenerator(4031, "", "CELLPHONE_NOT_EXIST", "1.0.0");
 
-const CELLPHONE_EXIST_IN_CONTACT = {
-	properties: { message: "", code: 4033, reason: "CELLPHONE_EXIST_IN_CONTACT" },
-	info: { version: "1.0.0" },
-};
+const CELLPHONE_REQUIRED = errorTemplateGenerator(4032, "", "CELLPHONE_REQUIRED", "1.0.0");
 
-const CONTACT_INVALID_TYPE = {
-	properties: { message: "", code: 4034, reason: "CONTACT_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const CELLPHONE_EXIST_IN_CONTACT = errorTemplateGenerator(
+	4033,
+	"",
+	"CELLPHONE_EXIST_IN_CONTACT",
+	"1.0.0",
+);
 
-const COUNTRY_CODE_INVALID_TYPE = {
-	properties: { message: "", code: 4035, reason: "COUNTRY_CODE_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const CONTACT_INVALID_TYPE = errorTemplateGenerator(4034, "", "CONTACT_INVALID_TYPE", "1.0.0");
 
-const COUNTRY_CODE_MAXLENGTH_REACH = {
-	properties: { message: "", code: 4036, reason: "COUNTRY_CODE_MAXLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const COUNTRY_CODE_INVALID_TYPE = errorTemplateGenerator(
+	4035,
+	"",
+	"COUNTRY_CODE_INVALID_TYPE",
+	"1.0.0",
+);
 
-const COUNTRY_CODE_MINLENGTH_REACH = {
-	properties: { message: "", code: 4037, reason: "COUNTRY_CODE_MINLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const COUNTRY_CODE_MAXLENGTH_REACH = errorTemplateGenerator(
+	4036,
+	"",
+	"COUNTRY_CODE_MAXLENGTH_REACH",
+	"1.0.0",
+);
 
-const COUNTRY_CODE_REQUIRED = {
-	properties: { message: "", code: 4038, reason: "COUNTRY_CODE_REQUIRED" },
-	info: { version: "1.0.0" },
-};
+const COUNTRY_CODE_MINLENGTH_REACH = errorTemplateGenerator(
+	4037,
+	"",
+	"COUNTRY_CODE_MINLENGTH_REACH",
+	"1.0.0",
+);
 
-const COUNTRY_NAME_INVALID_TYPE = {
-	properties: { message: "", code: 4039, reason: "COUNTRY_NAME_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const COUNTRY_CODE_REQUIRED = errorTemplateGenerator(
+	4038,
+	"",
+	"COUNTRY_CODE_REQUIRED",
+	"1.0.0",
+);
 
-const COUNTRY_NAME_MAXLENGTH_REACH = {
-	properties: { message: "", code: 4040, reason: "COUNTRY_NAME_MAXLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const COUNTRY_NAME_INVALID_TYPE = errorTemplateGenerator(
+	4039,
+	"",
+	"COUNTRY_NAME_INVALID_TYPE",
+	"1.0.0",
+);
 
-const COUNTRY_NAME_MINLENGTH_REACH = {
-	properties: { message: "", code: 4041, reason: "COUNTRY_NAME_MINLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const COUNTRY_NAME_MAXLENGTH_REACH = errorTemplateGenerator(
+	4040,
+	"",
+	"COUNTRY_NAME_MAXLENGTH_REACH",
+	"1.0.0",
+);
 
-const COUNTRY_NAME_REQUIRED = {
-	properties: { message: "", code: 4042, reason: "COUNTRY_NAME_REQUIRED" },
-	info: { version: "1.0.0" },
-};
+const COUNTRY_NAME_MINLENGTH_REACH = errorTemplateGenerator(
+	4041,
+	"",
+	"COUNTRY_NAME_MINLENGTH_REACH",
+	"1.0.0",
+);
 
-const CREATED_AT_INVALID_TYPE = {
-	properties: { message: "", code: 4043, reason: "CREATED_AT_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const COUNTRY_NAME_REQUIRED = errorTemplateGenerator(
+	4042,
+	"",
+	"COUNTRY_NAME_REQUIRED",
+	"1.0.0",
+);
 
-const FIRST_NAME_INVALID_TYPE = {
-	properties: { message: "", code: 4044, reason: "FIRST_NAME_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const CREATED_AT_INVALID_TYPE = errorTemplateGenerator(
+	4043,
+	"",
+	"CREATED_AT_INVALID_TYPE",
+	"1.0.0",
+);
 
-const FIRST_NAME_MAXLENGTH_REACH = {
-	properties: { message: "", code: 4045, reason: "FIRST_NAME_MAXLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const FIRST_NAME_INVALID_TYPE = errorTemplateGenerator(
+	4044,
+	"",
+	"FIRST_NAME_INVALID_TYPE",
+	"1.0.0",
+);
 
-const FIRST_NAME_MINLENGTH_REACH = {
-	properties: { message: "", code: 4046, reason: "FIRST_NAME_MINLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const FIRST_NAME_MAXLENGTH_REACH = errorTemplateGenerator(
+	4045,
+	"",
+	"FIRST_NAME_MAXLENGTH_REACH",
+	"1.0.0",
+);
 
-const FIRST_NAME_REQUIRED = {
-	properties: { message: "", code: 4047, reason: "FIRST_NAME_REQUIRED" },
-	info: { version: "1.0.0" },
-};
+const FIRST_NAME_MINLENGTH_REACH = errorTemplateGenerator(
+	4046,
+	"",
+	"FIRST_NAME_MINLENGTH_REACH",
+	"1.0.0",
+);
 
-const LAST_NAME_INVALID_TYPE = {
-	properties: { message: "", code: 4048, reason: "LAST_NAME_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const FIRST_NAME_REQUIRED = errorTemplateGenerator(4047, "", "FIRST_NAME_REQUIRED", "1.0.0");
 
-const LAST_NAME_MAXLENGTH_REACH = {
-	properties: { message: "", code: 4049, reason: "LAST_NAME_MAXLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const LAST_NAME_INVALID_TYPE = errorTemplateGenerator(
+	4048,
+	"",
+	"LAST_NAME_INVALID_TYPE",
+	"1.0.0",
+);
 
-const LAST_NAME_MINLENGTH_REACH = {
-	properties: { message: "", code: 4050, reason: "LAST_NAME_MINLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const LAST_NAME_MAXLENGTH_REACH = errorTemplateGenerator(
+	4049,
+	"",
+	"LAST_NAME_MAXLENGTH_REACH",
+	"1.0.0",
+);
 
-const MAC_ADDRESS_EXIST = {
-	properties: { message: "", code: 4051, reason: "MAC_ADDRESS_EXIST" },
-	info: { version: "1.0.0" },
-};
+const LAST_NAME_MINLENGTH_REACH = errorTemplateGenerator(
+	4050,
+	"",
+	"LAST_NAME_MINLENGTH_REACH",
+	"1.0.0",
+);
 
-const MAC_ADDRESS_INVALID_TYPE = {
-	properties: { message: "", code: 4052, reason: "MAC_ADDRESS_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const MAC_ADDRESS_EXIST = errorTemplateGenerator(4051, "", "MAC_ADDRESS_EXIST", "1.0.0");
 
-const MAC_ADDRESS_MAXLENGTH_REACH = {
-	properties: { message: "", code: 4053, reason: "MAC_ADDRESS_MAXLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const MAC_ADDRESS_INVALID_TYPE = errorTemplateGenerator(
+	4052,
+	"",
+	"MAC_ADDRESS_INVALID_TYPE",
+	"1.0.0",
+);
 
-const MAC_ADDRESS_MINLENGTH_REACH = {
-	properties: { message: "", code: 4054, reason: "MAC_ADDRESS_MINLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const MAC_ADDRESS_MAXLENGTH_REACH = errorTemplateGenerator(
+	4053,
+	"",
+	"MAC_ADDRESS_MAXLENGTH_REACH",
+	"1.0.0",
+);
 
-const MAC_ADDRESS_REQUIRED = {
-	properties: { message: "", code: 4055, reason: "MAC_ADDRESS_REQUIRED" },
-	info: { version: "1.0.0" },
-};
+const MAC_ADDRESS_MINLENGTH_REACH = errorTemplateGenerator(
+	4054,
+	"",
+	"MAC_ADDRESS_MINLENGTH_REACH",
+	"1.0.0",
+);
 
-const PHONE_NUMBER_INVALID_TYPE = {
-	properties: { message: "", code: 4056, reason: "PHONE_NUMBER_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const MAC_ADDRESS_REQUIRED = errorTemplateGenerator(4055, "", "MAC_ADDRESS_REQUIRED", "1.0.0");
 
-const PHONE_NUMBER_EXIST = {
-	properties: { message: "", code: 4057, reason: "PHONE_NUMBER_EXIST" },
-	info: { version: "1.0.0" },
-};
+const PHONE_NUMBER_INVALID_TYPE = errorTemplateGenerator(
+	4056,
+	"",
+	"PHONE_NUMBER_INVALID_TYPE",
+	"1.0.0",
+);
 
-const PHONE_NUMBER_REQUIRED = {
-	properties: { message: "", code: 4058, reason: "PHONE_NUMBER_REQUIRED" },
-	info: { version: "1.0.0" },
-};
+const PHONE_NUMBER_EXIST = errorTemplateGenerator(4057, "", "PHONE_NUMBER_EXIST", "1.0.0");
 
-const PHONE_NUMBER_MINLENGTH_REACH = {
-	properties: { message: "", code: 4059, reason: "PHONE_NUMBER_MINLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const PHONE_NUMBER_REQUIRED = errorTemplateGenerator(
+	4058,
+	"",
+	"PHONE_NUMBER_REQUIRED",
+	"1.0.0",
+);
 
-const PHONE_NUMBER_MAXLENGTH_REACH = {
-	properties: { message: "", code: 4060, reason: "PHONE_NUMBER_MAXLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const PHONE_NUMBER_MINLENGTH_REACH = errorTemplateGenerator(
+	4059,
+	"",
+	"PHONE_NUMBER_MINLENGTH_REACH",
+	"1.0.0",
+);
 
-const PRIVATE_ID_EXIST = {
-	properties: { message: "", code: 4061, reason: "PRIVATE_ID_EXIST" },
-	info: { version: "1.0.0" },
-};
+const PHONE_NUMBER_MAXLENGTH_REACH = errorTemplateGenerator(
+	4060,
+	"",
+	"PHONE_NUMBER_MAXLENGTH_REACH",
+	"1.0.0",
+);
 
-const PRIVATE_ID_INVALID_TYPE = {
-	properties: { message: "", code: 4062, reason: "PRIVATE_ID_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const PRIVATE_ID_EXIST = errorTemplateGenerator(4061, "", "PRIVATE_ID_EXIST", "1.0.0");
 
-const PRIVATE_ID_MAX_LENGTH_REACH = {
-	properties: { message: "", code: 4063, reason: "PRIVATE_ID_MAX_LENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const PRIVATE_ID_INVALID_TYPE = errorTemplateGenerator(
+	4062,
+	"",
+	"PRIVATE_ID_INVALID_TYPE",
+	"1.0.0",
+);
 
-const PRIVATE_ID_MIN_LENGTH_REACH = {
-	properties: { message: "", code: 4064, reason: "PRIVATE_ID_MIN_LENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const PRIVATE_ID_MAX_LENGTH_REACH = errorTemplateGenerator(
+	4063,
+	"",
+	"PRIVATE_ID_MAX_LENGTH_REACH",
+	"1.0.0",
+);
 
-const PRIVATE_ID_REQUIRED = {
-	properties: { message: "", code: 4065, reason: "PRIVATE_ID_REQUIRED" },
-	info: { version: "1.0.0" },
-};
+const PRIVATE_ID_MIN_LENGTH_REACH = errorTemplateGenerator(
+	4064,
+	"",
+	"PRIVATE_ID_MIN_LENGTH_REACH",
+	"1.0.0",
+);
+
+const PRIVATE_ID_REQUIRED = errorTemplateGenerator(4065, "", "PRIVATE_ID_REQUIRED", "1.0.0");
 
 const SELF_STUFF = {
 	properties: { message: "", code: 4066, reason: "SELF_STUFF" },
 	info: { version: "1.0.0" },
 };
 
-const TOKEN_EXIST = {
-	properties: { message: "", code: 4067, reason: "TOKEN_EXIST" },
-	info: { version: "1.0.0" },
-};
+errorTemplateGenerator(4066, "", "SELF_STUFF", "1.0.0");
 
-const TOKEN_REQUIRED = {
-	properties: { message: "", code: 4068, reason: "TOKEN_REQUIRED" },
-	info: { version: "1.0.0" },
-};
+const TOKEN_EXIST = errorTemplateGenerator(4067, "", "TOKEN_EXIST", "1.0.0");
 
-const TOKEN_INVALID_TYPE = {
-	properties: { message: "", code: 4069, reason: "TOKEN_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const TOKEN_REQUIRED = errorTemplateGenerator(4068, "", "TOKEN_REQUIRED", "1.0.0");
 
-const USER_NOT_EXIST = {
-	properties: {
-		message: "user not exist, maybe privateID is incorrect",
-		code: 4070,
-		reason: "USER_NOT_EXIST",
-	},
-	info: { version: "1.0.0" },
-};
+const TOKEN_INVALID_TYPE = errorTemplateGenerator(4069, "", "TOKEN_INVALID_TYPE", "1.0.0");
 
-const USERNAME_EXIST = {
-	properties: { message: "", code: 4071, reason: "USERNAME_EXIST" },
-	info: { version: "1.0.0" },
-};
+const USER_NOT_EXIST = errorTemplateGenerator(
+	4070,
+	"user not exist, maybe privateID is incorrect",
+	"USER_NOT_EXIST",
+	"1.0.0",
+);
 
-const USERNAME_INVALID_TYPE = {
-	properties: { message: "", code: 4072, reason: "USERNAME_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const USERNAME_EXIST = errorTemplateGenerator(4071, "", "USERNAME_EXIST", "1.0.0");
 
-const USERNAME_MAXLENGTH_REACH = {
-	properties: { message: "", code: 4073, reason: "USERNAME_MAXLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const USERNAME_INVALID_TYPE = errorTemplateGenerator(
+	4072,
+	"",
+	"USERNAME_INVALID_TYPE",
+	"1.0.0",
+);
 
-const USERNAME_MINLENGTH_REACH = {
-	properties: { message: "", code: 4074, reason: "USERNAME_MINLENGTH_REACH" },
-	info: { version: "1.0.0" },
-};
+const USERNAME_MAXLENGTH_REACH = errorTemplateGenerator(
+	4073,
+	"",
+	"USERNAME_MAXLENGTH_REACH",
+	"1.0.0",
+);
 
-const VERIFICATION_CODE_INVALID = {
-	properties: {
-		message: "Wrong verification code",
-		code: 4075,
-		reason: "VERIFICATION_CODE_INVALID",
-	},
-	info: { version: "1.0.0" },
-};
+const USERNAME_MINLENGTH_REACH = errorTemplateGenerator(
+	4074,
+	"",
+	"USERNAME_MINLENGTH_REACH",
+	"1.0.0",
+);
 
-const VERIFICATION_CODE_INVALID_TYPE = {
-	properties: { message: "", code: 4076, reason: "VERIFICATION_CODE_INVALID_TYPE" },
-	info: { version: "1.0.0" },
-};
+const VERIFICATION_CODE_INVALID = errorTemplateGenerator(
+	4075,
+	"Wrong verification code",
+	"VERIFICATION_CODE_INVALID",
+	"1.0.0",
+);
 
-const VERIFICATION_CODE_INVALID_LENGTH = {
-	properties: { message: "", code: 4077, reason: "VERIFICATION_CODE_INVALID_LENGTH" },
-	info: { version: "1.0.0" },
-};
+const VERIFICATION_CODE_INVALID_TYPE = errorTemplateGenerator(
+	4076,
+	"",
+	"VERIFICATION_CODE_INVALID_TYPE",
+	"1.0.0",
+);
+
+const VERIFICATION_CODE_INVALID_LENGTH = errorTemplateGenerator(
+	4077,
+	"",
+	"VERIFICATION_CODE_INVALID_LENGTH",
+	"1.0.0",
+);
 
 const userErrorTemplate = {
-	info: { version: "1.0.0" },
+	version: "1.0.0",
 
-	BLACKLIST_INVALID_TYPE,
 	BIO_INVALID_TYPE,
 	BIO_MAXLENGTH_REACH,
 	BIO_MINLENGTH_REACH,
+	BLACKLIST_INVALID_TYPE,
+	CELLPHONE_EXIST_IN_CONTACT,
 	CELLPHONE_EXIST,
 	CELLPHONE_INVALID_TYPE,
 	CELLPHONE_MAXLENGTH_REACH,
 	CELLPHONE_MINLENGTH_REACH,
 	CELLPHONE_NOT_EXIST,
 	CELLPHONE_REQUIRED,
-	CELLPHONE_EXIST_IN_CONTACT,
 	CONTACT_INVALID_TYPE,
 	COUNTRY_CODE_INVALID_TYPE,
 	COUNTRY_CODE_MAXLENGTH_REACH,
@@ -317,11 +336,11 @@ const userErrorTemplate = {
 	MAC_ADDRESS_MAXLENGTH_REACH,
 	MAC_ADDRESS_MINLENGTH_REACH,
 	MAC_ADDRESS_REQUIRED,
-	PHONE_NUMBER_INVALID_TYPE,
 	PHONE_NUMBER_EXIST,
-	PHONE_NUMBER_REQUIRED,
-	PHONE_NUMBER_MINLENGTH_REACH,
+	PHONE_NUMBER_INVALID_TYPE,
 	PHONE_NUMBER_MAXLENGTH_REACH,
+	PHONE_NUMBER_MINLENGTH_REACH,
+	PHONE_NUMBER_REQUIRED,
 	PRIVATE_ID_EXIST,
 	PRIVATE_ID_INVALID_TYPE,
 	PRIVATE_ID_MAX_LENGTH_REACH,
@@ -329,74 +348,18 @@ const userErrorTemplate = {
 	PRIVATE_ID_REQUIRED,
 	SELF_STUFF,
 	TOKEN_EXIST,
-	TOKEN_REQUIRED,
 	TOKEN_INVALID_TYPE,
+	TOKEN_REQUIRED,
 	USER_NOT_EXIST,
 	USERNAME_EXIST,
 	USERNAME_INVALID_TYPE,
 	USERNAME_MAXLENGTH_REACH,
 	USERNAME_MINLENGTH_REACH,
-	VERIFICATION_CODE_INVALID,
-	VERIFICATION_CODE_INVALID_TYPE,
 	VERIFICATION_CODE_INVALID_LENGTH,
+	VERIFICATION_CODE_INVALID_TYPE,
+	VERIFICATION_CODE_INVALID,
 };
 
 module.exports = {
 	userErrorTemplate,
-
-	BLACKLIST_INVALID_TYPE,
-	BIO_INVALID_TYPE,
-	BIO_MAXLENGTH_REACH,
-	BIO_MINLENGTH_REACH,
-	CELLPHONE_EXIST_IN_CONTACT,
-	CELLPHONE_EXIST,
-	CELLPHONE_INVALID_TYPE,
-	CELLPHONE_MAXLENGTH_REACH,
-	CELLPHONE_MINLENGTH_REACH,
-	CELLPHONE_NOT_EXIST,
-	CELLPHONE_REQUIRED,
-	CONTACT_INVALID_TYPE,
-	COUNTRY_CODE_INVALID_TYPE,
-	COUNTRY_CODE_MAXLENGTH_REACH,
-	COUNTRY_CODE_MINLENGTH_REACH,
-	COUNTRY_CODE_REQUIRED,
-	COUNTRY_NAME_INVALID_TYPE,
-	COUNTRY_NAME_MAXLENGTH_REACH,
-	COUNTRY_NAME_MINLENGTH_REACH,
-	COUNTRY_NAME_REQUIRED,
-	CREATED_AT_INVALID_TYPE,
-	FIRST_NAME_INVALID_TYPE,
-	FIRST_NAME_MAXLENGTH_REACH,
-	FIRST_NAME_MINLENGTH_REACH,
-	FIRST_NAME_REQUIRED,
-	LAST_NAME_INVALID_TYPE,
-	LAST_NAME_MAXLENGTH_REACH,
-	LAST_NAME_MINLENGTH_REACH,
-	MAC_ADDRESS_EXIST,
-	MAC_ADDRESS_INVALID_TYPE,
-	MAC_ADDRESS_MAXLENGTH_REACH,
-	MAC_ADDRESS_MINLENGTH_REACH,
-	MAC_ADDRESS_REQUIRED,
-	PHONE_NUMBER_EXIST,
-	PHONE_NUMBER_INVALID_TYPE,
-	PHONE_NUMBER_MAXLENGTH_REACH,
-	PHONE_NUMBER_MINLENGTH_REACH,
-	PHONE_NUMBER_REQUIRED,
-	PRIVATE_ID_EXIST,
-	PRIVATE_ID_INVALID_TYPE,
-	PRIVATE_ID_MAX_LENGTH_REACH,
-	PRIVATE_ID_MIN_LENGTH_REACH,
-	PRIVATE_ID_REQUIRED,
-	SELF_STUFF,
-	TOKEN_EXIST,
-	TOKEN_INVALID_TYPE,
-	TOKEN_REQUIRED,
-	USER_NOT_EXIST,
-	USERNAME_EXIST,
-	USERNAME_INVALID_TYPE,
-	USERNAME_MAXLENGTH_REACH,
-	USERNAME_MINLENGTH_REACH,
-	VERIFICATION_CODE_INVALID_LENGTH,
-	VERIFICATION_CODE_INVALID_TYPE,
-	VERIFICATION_CODE_INVALID,
 };

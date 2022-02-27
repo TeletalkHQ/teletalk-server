94a3272b2ccb12dd773399f542905b9c6d4a3d25
S-STALWART-S <stalwart.programmer@gmail.com>
2022-02-28 01:31:51 +0330

    added: [ utils:{ schemaPropertyKeyGenerator, schemaTemplateGenerator }, ]

--- src/function/utility/generators.js
+++ src/function/utility/generators.js
@@ -13,7 +13,7 @@ const errorTemplateGenerator = (code, message, reason, version) => {
 		}
 		return { code, message, reason, version };
 	} catch (error) {
-		console.log("templateGenerator catch", error);
+		console.log("templateGenerator catch, error:", error);
 	}
 };
 
@@ -31,10 +31,65 @@ const routeTemplateGenerator = (method, route, version, description = "") => {
 		}
 		return { method, route, version, description };
 	} catch (error) {
-		console.log("templateGenerator catch", error);
+		console.log("templateGenerator catch, error:", error);
 	}
 };
 
 const mongooseSchemaGenerator = () => {};
 
-module.exports = { errorTemplateGenerator, routeTemplateGenerator, mongooseSchemaGenerator };
+const schemaTemplateGenerator = (
+	maxlength,
+	minlength,
+	required,
+	trim,
+	type,
+	unique,
+	defaultValue,
+	version,
+	lowercase,
+	length,
+) => {
+	try {
+		return {
+			maxlength,
+			minlength,
+			required,
+			trim,
+			type,
+			unique,
+			default: defaultValue,
+			version,
+			lowercase,
+			length,
+		};
+	} catch (error) {
+		logger.log("schemaTemplateGenerator catch, error:", error);
+	}
+};
+
+const schemaPropertyKeyGenerator = (
+	value,
+	error = { reason: "Not set!", message: "Not set!", code: "Not set!", version: "Not set!" },
+) => {
+	if (typeof value === "undefined") {
+		const error = "Value need to be set!";
+		throw error;
+	}
+
+	try {
+		return {
+			value,
+			error,
+		};
+	} catch (error) {
+		logger.log("schemaPropertyKeyGenerator catch, error:", error);
+	}
+};
+
+module.exports = {
+	errorTemplateGenerator,
+	mongooseSchemaGenerator,
+	routeTemplateGenerator,
+	schemaPropertyKeyGenerator,
+	schemaTemplateGenerator,
+};

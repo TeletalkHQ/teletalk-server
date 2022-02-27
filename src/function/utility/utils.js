f4b077600f9c19e7b7b96f04b44b10cc77accfd9
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-02-27 23:09:06 +0330

    added: [utils: { generators } ]

---
+++ src/function/utility/utils.js
@@ -0,0 +1,15 @@
+const objectInitializer = (values, props) => {
+	try {
+		const tempObj = {};
+
+		props.forEach((prop, index) => {
+			tempObj[prop] = values[index];
+		});
+
+		return tempObj;
+	} catch (error) {
+		console.log("objectInitializer catch", error);
+	}
+};
+
+module.exports = { objectInitializer };

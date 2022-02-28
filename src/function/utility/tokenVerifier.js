b4c80151bd9b090b6b945937042cfee0764096c5
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-03-01 00:03:11 +0330

    some little changes on objectClarify

--- src/function/utility/tokenVerifier.js
+++ src/function/utility/tokenVerifier.js
@@ -2,7 +2,7 @@ const JWT = require("jsonwebtoken");
 
 const { initialValue } = require("~/constant/initialValue/initialValue");
 
-const initialOptions = initialValue.JWTOptions;
+const initialOptions = initialValue.jwtOptions;
 
 const tokenVerifier = async ({ token, secret, options = initialOptions }) => {
 	const data = JWT.verify(token, secret || process.env.JWT_MAIN_SECRET, {

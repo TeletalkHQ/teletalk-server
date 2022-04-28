c85eeb8f7aa7fa8cb46b44e9c6c7edf5ff9714a0
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-04-28 23:41:27 +0430

    routers relocated

--- test/test.js
+++ test/test.js
@@ -1,4 +1,6 @@
+// * Test api's
+require("@/apis/testApis/testApi.spec");
 // * User api's
-require("@/apis/userApis/signInNormalApi.spec");
-require("@/apis/userApis/verifySignInNormalApi.spec");
-require("@/apis/userApis/createNewUserApi.spec");
+// require("@/apis/userApis/signInNormalApi.spec");
+// require("@/apis/userApis/verifySignInNormalApi.spec");
+// require("@/apis/userApis/createNewUserApi.spec");

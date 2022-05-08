80c107000c143611f07244a4b93322735c1536e5
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-05-08 18:05:55 +0430

    editContactApi fixed (need more)

--- test/requirements.spec.js
+++ test/requirements.spec.js
@@ -30,7 +30,7 @@ describe("Add requirements to application state", () => {
       c.countryName.toLowerCase().includes("iran")
     );
 
-    const users = Array.from({ length: 5 });
+    const users = Array.from({ length: 50 });
 
     const testUsers = {};
     for (let i = 0; i < users.length; i++) {

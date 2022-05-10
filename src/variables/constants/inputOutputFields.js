2c90c943b1fd6c9da5c3c6b1fa6f8fa4c022291f
S-STALWART-S <stalwart.programmer@gmail.com>
2022-05-10 18:53:22 +0430

    null

---
+++ src/variables/constants/inputOutputFields.js
@@ -0,0 +1,16 @@
+const { userModels } = require("@/models/userModels/userModels");
+
+const inputFields = userModels;
+
+const outputFields = {};
+
+const inputOutputFields = {
+  inputFields,
+  outputFields,
+};
+
+module.exports = {
+  inputFields,
+  inputOutputFields,
+  outputFields,
+};

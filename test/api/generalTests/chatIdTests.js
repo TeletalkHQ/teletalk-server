138803927c954a05a98bb9469f97a1b8ca9b0d50
S-STALWART-S <stalwart.programmer@gmail.com>
2022-06-07 01:13:46 +0430

    added:[ chatIdFailureTests ]

--- test/api/generalTests/chatIdTests.js
+++ test/api/generalTests/chatIdTests.js
@@ -1,7 +1,19 @@
+const { customRequest } = require("@/functions/helpers/CustomRequest");
+const { randomMaker } = require("@/functions/helpers/RandomMaker");
 const { testBuilder } = require("@/functions/testUtilities/TestBuilder");
+
 const {
   chatModels: { chatIdModel },
 } = require("@/models/chatModels/chatModels");
+
+const {
+  chatErrors: {
+    CHAT_ID_INVALID_TYPE,
+    CHAT_ID_MAX_LENGTH_REACH,
+    CHAT_ID_MIN_LENGTH_REACH,
+    CHAT_ID_REQUIRED,
+  },
+} = require("@/variables/errors/chatErrors");
 
 const chatIdSuccessTests = (
   { chatIdMain, chatIdTest } = {},
@@ -17,8 +29,34 @@ const chatIdSuccessTests = (
     .execute();
 };
 
-const chatIdFailureTests = () => {};
+const chatIdMinLength = chatIdModel.minlength.value;
+const chatIdMaxLength = chatIdModel.maxlength.value;
 
+const chatIdFailureTests = (data = {}) => {
+  const fn = (chatId) => ({ ...data });
+
+  it("Should get error, CHAT_ID_REQUIRED", async () => {
+    await customRequest.sendRequest(fn(undefined), CHAT_ID_REQUIRED);
+  });
+
+  it("Should get error, CHAT_ID_INVALID_TYPE", async () => {
+    await customRequest.sendRequest(fn(12365475), CHAT_ID_INVALID_TYPE);
+  });
+
+  it("Should get error, CHAT_ID_MAX_LENGTH_REACH", async () => {
+    await customRequest.sendRequest(
+      fn(randomMaker.randomString(+chatIdMaxLength + 1)),
+      CHAT_ID_MAX_LENGTH_REACH
+    );
+  });
+
+  it("Should get error, CHAT_ID_MIN_LENGTH_REACH", async () => {
+    await customRequest.sendRequest(
+      fn(randomMaker.randomString(+chatIdMinLength - 1)),
+      CHAT_ID_MIN_LENGTH_REACH
+    );
+  });
+};
 module.exports = {
   chatIdSuccessTests,
   chatIdFailureTests,

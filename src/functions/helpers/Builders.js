1868a788b4ffe47e892f798618883e75073aea7d
S-STALWART-S <dev.soheil.ss@gmail.com>
2022-06-09 23:37:52 +0430

    cleanup:[ builder classes ]

--- src/functions/helpers/Builders.js
+++ src/functions/helpers/Builders.js
@@ -9,7 +9,11 @@ const {
 
 class RouteBuilder {
   constructor() {
-    this.defaultRouteObject = {
+    this.routeObject = this.#defaultRouteObject();
+  }
+
+  #defaultRouteObject() {
+    return {
       method: "GET",
       url: "/404",
       statusCode: 404,
@@ -18,70 +22,55 @@ class RouteBuilder {
       inputFields: [{}],
       outputFields: [{}],
     };
-
-    this.routeObject = { ...this.defaultRouteObject };
+  }
+  #addProperty(key, value) {
+    this.routeObject[key] = value;
   }
 
   create() {
     this.routeObject = {};
-
     return this;
   }
-
   build() {
     return this.routeObject;
   }
-
-  addProperty(key, value) {
-    this.routeObject[key] = value;
-  }
-
   method(method) {
-    this.addProperty("method", method);
-
+    this.#addProperty("method", method);
     return this;
   }
-
   url(url) {
-    this.addProperty("url", url);
-
+    this.#addProperty("url", url);
     return this;
   }
-
   statusCode(statusCode) {
-    this.addProperty("statusCode", statusCode);
-
+    this.#addProperty("statusCode", statusCode);
     return this;
   }
-
   version(version) {
-    this.addProperty("version", version);
-
+    this.#addProperty("version", version);
     return this;
   }
-
   description(description) {
-    this.addProperty("description", description);
-
+    this.#addProperty("description", description);
     return this;
   }
-
   inputFields(inputFields) {
-    this.addProperty("inputFields", inputFields);
-
+    this.#addProperty("inputFields", inputFields);
     return this;
   }
-
   outputFields(outputFields) {
-    this.addProperty("outputFields", outputFields);
-
+    this.#addProperty("outputFields", outputFields);
     return this;
   }
 }
 
 class ErrorBuilder {
   constructor() {
-    this.defaultErrorObject = {
+    this.errorObject = this.#defaultErrorObject();
+  }
+
+  #defaultErrorObject() {
+    return {
       description: "Default route description",
       message: "",
       reason: "UNKNOWN_ERROR",
@@ -90,78 +79,54 @@ class ErrorBuilder {
       errorKey: "",
       errorCode: 4000,
     };
-    this.errorObject = { ...this.defaultErrorObject };
+  }
+  #addProperty(key, value) {
+    this.errorObject[key] = value;
   }
 
   create() {
     this.errorObject = {};
-
     return this;
   }
-
   build() {
     return this.errorObject;
   }
-
-  addProperty(key, value) {
-    this.errorObject[key] = value;
-  }
-
   errorCode(errorCode) {
-    this.addProperty("errorCode", errorCode);
-
+    this.#addProperty("errorCode", errorCode);
     return this;
   }
-
   statusCode(statusCode) {
-    this.addProperty("statusCode", statusCode);
-
+    this.#addProperty("statusCode", statusCode);
     return this;
   }
-
   message(message) {
-    this.addProperty("message", message);
-
+    this.#addProperty("message", message);
     return this;
   }
-
   errorReason(errorReason) {
-    this.addProperty("reason", errorReason);
-
+    this.#addProperty("reason", errorReason);
     return this;
   }
-
   errorKey(errorKey) {
-    this.addProperty("errorKey", errorKey);
-
+    this.#addProperty("errorKey", errorKey);
     return this;
   }
-
   version(version) {
     this.errorObject.version = version;
-
     return this;
   }
 }
 
 class ModelBuilder {
   constructor() {
-    this.modelObject = this.setDefaultModelObject();
+    this.modelObject = this.#defaultModelObject();
   }
 
-  create() {
-    this.modelObject = this.setDefaultModelObject();
-
-    return this;
-  }
-
-  addProperty(key, value, error) {
+  #addProperty(key, value, error) {
     this.modelObject[key].value = value;
-
     if (error) this.modelObject[key].error = error;
   }
-
-  setDefaultModelObject() {
+  #defaultModelObject() {
     return {
       defaultValue: modelPropertyGenerator("", {}),
       empty: modelPropertyGenerator(false, {}),
@@ -178,190 +143,77 @@ class ModelBuilder {
     };
   }
 
+  create() {
+    this.modelObject = this.#defaultModelObject();
+    return this;
+  }
   build() {
     return this.modelObject;
   }
-
   maxlength(value, error) {
-    this.addProperty("maxlength", value, error);
-
+    this.#addProperty("maxlength", value, error);
     return this;
   }
-
   minlength(value, error) {
-    this.addProperty("minlength", value, error);
-
+    this.#addProperty("minlength", value, error);
     return this;
   }
-
   numeric(value, error) {
-    this.addProperty("numeric", value, error);
-
+    this.#addProperty("numeric", value, error);
     return this;
   }
-
   type(value, error) {
-    this.addProperty("type", value, error);
-
+    this.#addProperty("type", value, error);
     return this;
   }
-
   empty(value, error) {
-    this.addProperty("empty", value, error);
-
+    this.#addProperty("empty", value, error);
     return this;
   }
-
   version(value) {
     this.modelObject.version = value;
-
     return this;
   }
-
   required(value, error = {}) {
-    this.addProperty("required", value, error);
-
+    this.#addProperty("required", value, error);
     return this;
   }
-
   trim(value, error) {
-    this.addProperty("trim", value, error);
-
+    this.#addProperty("trim", value, error);
     return this;
   }
-
   unique(value, error) {
-    this.addProperty("unique", value, error);
-
+    this.#addProperty("unique", value, error);
     return this;
   }
-
   defaultValue(value, error) {
-    this.addProperty("defaultValue", value, error);
-
+    this.#addProperty("defaultValue", value, error);
     return this;
   }
-
   lowercase(value, error) {
-    this.addProperty("lowercase", value, error);
-
+    this.#addProperty("lowercase", value, error);
     return this;
   }
-
   length(value, error) {
-    this.addProperty("length", value, error);
-
+    this.#addProperty("length", value, error);
     return this;
   }
 }
 
 class ValidationModelBuilder {
   constructor() {
-    this.validationModelObject = this.setDefaultValidationModelObject();
-    this.modelObject = {};
-  }
-  create() {
-    this.validationModelObject = this.setDefaultValidationModelObject();
-    this.modelObject = {};
-
-    return this;
+    this.validationModelObject = this.#defaultValidationModelObject();
+    this.modelObject = this.#defaultModelObject();
   }
 
-  build() {
-    return objectClarify(this.validationModelObject);
+  #reset() {
+    this.validationModelObject = this.#defaultValidationModelObject();
+    this.modelObject = this.#defaultModelObject();
   }
-
-  setModelObject(modelObject) {
-    this.modelObject = modelObject;
-
-    return this;
+  #defaultModelObject() {
+    return {};
   }
-
-  _addProperty(validationKey, modelKey, messageKey) {
-    this._setValue(validationKey, modelKey);
-    this._setMessage(modelKey, messageKey);
-  }
-  _addPropertyWithoutMessage(validationKey, modelKey) {
-    this._setValue(validationKey, modelKey);
-  }
-  _setValue(validationKey, modelKey) {
-    this.validationModelObject[validationKey] =
-      this.modelObject[modelKey].value;
-  }
-  _setMessage(modelKey, messageKey) {
-    this.validationModelObject.messages[messageKey] =
-      this.modelObject[modelKey].error.message;
-  }
-
-  empty() {
-    this._addProperty("empty", "empty", "stringEmpty");
-
-    return this;
-  }
-
-  length() {
-    this._addProperty("length", "length", "length");
-
-    return this;
-  }
-
-  max() {
-    this._addProperty("max", "maxlength", "stringMax");
-
-    return this;
-  }
-
-  min() {
-    this._addProperty("min", "minlength", "stringMin");
-
-    return this;
-  }
-
-  numeric() {
-    this._addProperty("numeric", "numeric", "stringNumeric");
-
-    return this;
-  }
-
-  trim() {
-    this._addPropertyWithoutMessage("trim", "trim");
-
-    return this;
-  }
-
-  type() {
-    this._addProperty("type", "type", "string");
-
-    return this;
-  }
-
-  unique() {
-    this._addProperty("unique", "unique", "unique");
-
-    return this;
-  }
-
-  required() {
-    this._addProperty("required", "required", "required");
-
-    return this;
-  }
-
-  //CLEANME ValidationModelBuilder optional method
-  optional() {
-    this.validationModelObject.optional = !this.modelObject.required.value;
-
-    return this;
-  }
-
-  //CLEANME ValidationModelBuilder lowercase method
-  lowercase() {
-    this.validationModelObject.lowercase = !this.modelObject.lowercase.value;
-
-    return this;
-  }
-
-  setDefaultValidationModelObject() {
+  #defaultValidationModelObject() {
     return {
       empty: undefined,
       max: undefined,
@@ -382,44 +234,103 @@ class ValidationModelBuilder {
       },
     };
   }
+  #addProperty(validationKey, modelKey, messageKey) {
+    this.#setValue(validationKey, modelKey);
+    this.#setMessage(modelKey, messageKey);
+  }
+  #addPropertyWithoutMessage(validationKey, modelKey) {
+    this.#setValue(validationKey, modelKey);
+  }
+  #setValue(validationKey, modelKey) {
+    this.validationModelObject[validationKey] =
+      this.modelObject[modelKey].value;
+  }
+  #setMessage(modelKey, messageKey) {
+    this.validationModelObject.messages[messageKey] =
+      this.modelObject[modelKey].error.message;
+  }
+
+  create() {
+    this.#reset();
+    return this;
+  }
+  build() {
+    return objectClarify(this.validationModelObject);
+  }
+  setModelObject(modelObject) {
+    this.modelObject = modelObject;
+    return this;
+  }
+  empty() {
+    this.#addProperty("empty", "empty", "stringEmpty");
+    return this;
+  }
+  length() {
+    this.#addProperty("length", "length", "length");
+    return this;
+  }
+  max() {
+    this.#addProperty("max", "maxlength", "stringMax");
+    return this;
+  }
+  min() {
+    this.#addProperty("min", "minlength", "stringMin");
+    return this;
+  }
+  numeric() {
+    this.#addProperty("numeric", "numeric", "stringNumeric");
+    return this;
+  }
+  trim() {
+    this.#addPropertyWithoutMessage("trim", "trim");
+    return this;
+  }
+  type() {
+    this.#addProperty("type", "type", "string");
+    return this;
+  }
+  unique() {
+    this.#addProperty("unique", "unique", "unique");
+    return this;
+  }
+  required() {
+    this.#addProperty("required", "required", "required");
+    return this;
+  }
+  //CLEANME ValidationModelBuilder optional method
+  optional() {
+    this.validationModelObject.optional = !this.modelObject.required.value;
+    return this;
+  }
+  //CLEANME ValidationModelBuilder lowercase method
+  lowercase() {
+    this.validationModelObject.lowercase = !this.modelObject.lowercase.value;
+    return this;
+  }
 }
 
 class ValidationErrorBuilder {
   constructor() {
-    this.options = this.defaultOptions();
-    this.validationResultErrorKeys = this.defaultValidationResultErrorKeys();
+    this.options = this.#defaultOptions();
+    this.validationResultErrorKeys = this.#defaultValidationResultErrorKeys();
     this.validationResult = [];
-    this.makeErrorObject = this.defaultMakeErrorObject();
+    this.makeErrorObject = this.#defaultMakeErrorObject();
     this.errors = [];
   }
 
-  create() {
-    this.reset();
-
-    return this;
-  }
-
-  reset() {
-    this.options = this.defaultOptions();
-    this.makeErrorObject = this.defaultMakeErrorObject();
-    this.validationResultErrorKeys = this.defaultValidationResultErrorKeys();
-    this.validationResult = this.defaultValidationResult();
-    this.errors = [];
-  }
-
-  defaultOptions() {
+  #defaultOptions() {
     return {
       autoErrorDetection: true,
       extraErrorFields: {},
     };
   }
-  defaultValidationResultErrorKeys() {
+  #defaultValidationResultErrorKeys() {
     return getValidatorErrorTypes([]);
   }
-  defaultValidationResult() {
+  #defaultValidationResult() {
     return [];
   }
-  defaultMakeErrorObject() {
+  #defaultMakeErrorObject() {
     return (errorObject) => {
       return getErrorObject(errorObject, {
         validationResult: this.validationResult,
@@ -427,14 +338,44 @@ class ValidationErrorBuilder {
       });
     };
   }
+  #setOptions(options = this.options) {
+    this.options = {
+      ...this.options,
+      ...options,
+    };
+    return this;
+  }
+  #setValidationResult(result) {
+    this.validationResult = result;
+    return this;
+  }
+  #setValidationErrorKeys(result) {
+    this.validationResultErrorKeys = getValidatorErrorTypes(result);
+    return this;
+  }
+  #reset() {
+    this.options = this.#defaultOptions();
+    this.makeErrorObject = this.#defaultMakeErrorObject();
+    this.validationResultErrorKeys = this.#defaultValidationResultErrorKeys();
+    this.validationResult = this.#defaultValidationResult();
+    this.errors = [];
+  }
+
+  create() {
+    this.#reset();
+    return this;
+  }
+  addError(condition, errorObject) {
+    this.errors.push({ condition, errorObject });
+    return this;
+  }
   setRequirements(result, options = this.options) {
-    this.setValidationResult(result);
-    if (customTypeof(result).type.array) this.setValidationErrorKeys(result);
-    this.setOptions(options);
+    this.#setValidationResult(result);
+    if (customTypeof(result).type.array) this.#setValidationErrorKeys(result);
+    this.#setOptions(options);
 
     return this;
   }
-
   execute() {
     for (const error of this.errors) {
       const { condition, errorObject } = error;
@@ -442,30 +383,8 @@ class ValidationErrorBuilder {
       errorThrower(condition, () => this.makeErrorObject(errorObject));
     }
   }
-
-  addError(condition, errorObject) {
-    this.errors.push({ condition, errorObject });
-
-    return this;
-  }
-  setValidationResult(result) {
-    this.validationResult = result;
-    return this;
-  }
-  setValidationErrorKeys(result) {
-    this.validationResultErrorKeys = getValidatorErrorTypes(result);
-    return this;
-  }
-  setOptions(options = this.options) {
-    this.options = {
-      ...this.options,
-      ...options,
-    };
-
-    return this;
-  }
   addExtraErrorFields(fields = {}) {
-    this.setOptions({
+    this.#setOptions({
       extraErrorFields: {
         ...this.options.extraErrorFields,
         ...fields,
@@ -474,7 +393,6 @@ class ValidationErrorBuilder {
 
     return this;
   }
-
   customCheck(condition, cb) {
     if (condition) cb();
     return this;
@@ -515,37 +433,14 @@ class ValidationErrorBuilder {
 
 class MongoModelBuilder {
   constructor() {
-    this.modelObject = {};
-    this.mongoModel = this.setDefaultMongoModel();
+    this.modelObject = this.#defaultModelObject();
+    this.mongoModel = this.#defaultMongoModel();
   }
 
-  create() {
-    this.mongoModel = this.setDefaultMongoModel();
-    this.modelObject = {};
-
-    return this;
+  #defaultModelObject() {
+    return {};
   }
-
-  build() {
-    const finalMongoModel = {};
-
-    Object.keys(this.mongoModel)
-      .map((key) => {
-        if (this.mongoModel[key].length > 0)
-          return { key, value: this.mongoModel[key] };
-        return undefined;
-      })
-      .forEach((item) => {
-        if (!item) return;
-
-        const { key, value } = item;
-        finalMongoModel[key] = value.length > 1 ? value : value[0];
-      });
-
-    return finalMongoModel;
-  }
-
-  setDefaultMongoModel() {
+  #defaultMongoModel() {
     return {
       defaultValue: [],
       lowercase: [],
@@ -557,63 +452,73 @@ class MongoModelBuilder {
       unique: [],
     };
   }
+  #reset() {
+    this.mongoModel = this.#defaultMongoModel();
+    this.modelObject = {};
+  }
+  #addProperty(name) {
+    this.#setProperty(name);
+    this.#setMessage(name);
+  }
+  #addPropertyWithoutMessage(name) {
+    this.#setProperty(name);
+  }
+  #setProperty(key) {
+    this.mongoModel[key].push(this.modelObject[key].value);
+  }
+  #setMessage(key) {
+    this.mongoModel[key].push(this.modelObject[key].error?.message);
+  }
 
-  setModelObject(modelObject) {
-    this.modelObject = modelObject;
-
+  create() {
+    this.#reset();
     return this;
   }
+  build() {
+    const finalMongoModel = {};
+    Object.keys(this.mongoModel).forEach((key) => {
+      if (this.mongoModel[key].length > 0) {
+        const value = this.mongoModel[key];
+        finalMongoModel[key] = value.length > 1 ? value : value[0];
+      }
+    });
 
-  addProperty(name, addMessageCondition) {
-    this.mongoModel[name].push(this.modelObject[name].value);
-
-    const errorMessage = this.modelObject[name].error?.message;
-
-    if (
-      !customTypeof(errorMessage).type.undefined &&
-      addMessageCondition === true
-    )
-      this.mongoModel[name].push(errorMessage);
+    return finalMongoModel;
   }
-
+  setModelObject(modelObject) {
+    this.modelObject = modelObject;
+    return this;
+  }
   defaultValue() {
-    this.addProperty("defaultValue");
-
+    this.#addProperty("defaultValue");
     return this;
   }
   lowercase() {
-    this.addProperty("lowercase");
-
+    this.#addProperty("lowercase");
     return this;
   }
   maxlength() {
-    this.addProperty("maxlength");
-
+    this.#addProperty("maxlength");
     return this;
   }
   minlength() {
-    this.addProperty("minlength");
-
+    this.#addProperty("minlength");
     return this;
   }
   required() {
-    this.addProperty("required");
-
+    this.#addProperty("required");
     return this;
   }
-  trim(addMessageCondition = false) {
-    this.addProperty("trim", addMessageCondition);
-
+  trim() {
+    this.#addPropertyWithoutMessage("trim");
     return this;
   }
-  type(addMessageCondition = false) {
-    this.addProperty("type", addMessageCondition);
-
+  type() {
+    this.#addPropertyWithoutMessage("type");
     return this;
   }
   unique() {
-    this.addProperty("unique");
-
+    this.#addProperty("unique");
     return this;
   }
 }

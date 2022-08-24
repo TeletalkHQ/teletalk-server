45c0f1385e82eaff2e0d847e5a6458bab5113659
S-STALWART-S <stalwart.programmer@gmail.com>
2022-08-24 17:15:15 +0430

    etc commit

--- src/controllers/versionControlControllers/getAllStuffVersionControlController.js
+++ src/controllers/versionControlControllers/getAllStuffVersionControlController.js
@@ -1,11 +1,24 @@
+const { enErrorMessages } = require("@/translate/messages/enErrorMessages");
+
 const { allStuff } = require("@/variables/others/allStuff");
+
+const languages = {
+  en: { errorMessages: enErrorMessages },
+};
 
 const getAllStuffVersionControlController = async (
   req = expressRequest,
   res = expressResponse
 ) => {
   try {
-    res.status(200).json(allStuff);
+    const { language = "en" } = req.body;
+
+    const languageData = languages[language];
+
+    logger.log("rm", languageData, "languageData");
+    logger.log("rm", language, "language");
+    //TODO Update with checkAndResponse ?!
+    res.status(200).json({ ...allStuff, languageData });
   } catch (error) {
     logger.log("getAllStuffVersionControlController catch, error:", error);
     res.errorCollector(error);

d0e687b8beb64f31f2e53f257d92c144a8c3d7b2
S-STALWART-S <stalwart.programmer@gmail.com>
2022-08-26 17:42:21 +0430

    added:[ updateUserData controller ]

---
+++ src/controllers/userControllers/updateUserDataUserController.js
@@ -0,0 +1,54 @@
+//UNUSED updateUserDataUserController
+//CLEANME updateUserDataUserController
+//REFACTOR updateUserDataUserController
+const { authManager } = require("@/classes/AuthManager");
+const { userPropsUtilities } = require("@/classes/UserPropsUtilities");
+const {
+  updateUserDataByPrivateId,
+  userFinder,
+} = require("@/models/userModels/userModelFunctions");
+const { tokenValidator } = require("@/validators/userValidators");
+
+const updateUserDataUserController = async (
+  req = expressRequest,
+  res = expressResponse
+) => {
+  try {
+    const {
+      body: { firstName, lastName },
+    } = req;
+
+    const verifyToken = authManager.getTokenFromRequest(req);
+    const verifiedToken = await tokenValidator(
+      verifyToken,
+      authManager.getJwtSignInSecret()
+    );
+    const cellphone = userPropsUtilities.extractCellphone(
+      verifiedToken.payload
+    );
+
+    const user = await userFinder(cellphone);
+
+    await updateUserDataByPrivateId({
+      tokens: user.token,
+      firstName,
+      lastName,
+      privateId: user.privateId,
+    });
+    res.checkDataAndResponse({
+      user: {
+        ...cellphone,
+        firstName,
+        lastName,
+        privateId: user.privateId,
+        mainToken: userPropsUtilities.getTokenFromUserObjectByParam(user),
+      },
+    });
+  } catch (error) {
+    logger.log("updateUserDataUserController catch, error:", error);
+    res.errorCollector(error);
+    res.errorResponser();
+  }
+};
+
+module.exports = { updateUserDataUserController };

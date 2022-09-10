a97d117d6a7b524c4243ff30cc648fe66d6b7020
S-STALWART-S <stalwart.programmer@gmail.com>
2022-09-11 00:50:02 +0430

    refactored:[ createNewUserController with Trier ]

--- src/controllers/userControllers/createNewUserUserController.js
+++ src/controllers/userControllers/createNewUserUserController.js
@@ -2,8 +2,10 @@ const {
   objectUtilities,
 } = require("utility-store/src/classes/ObjectUtilities");
 const { randomMaker } = require("utility-store/src/classes/RandomMaker");
+const { trier } = require("utility-store/src/classes/Trier");
 
 const { authManager } = require("@/classes/AuthManager");
+const { commonFunctionalities } = require("@/classes/CommonFunctionalities");
 const { temporaryClients } = require("@/classes/TemporaryClients");
 const { userPropsUtilities } = require("@/classes/UserPropsUtilities");
 
@@ -18,83 +20,157 @@ const {
 } = require("@/models/commonModels/commonModels");
 
 const {
-  tokenValidator,
   firstNameValidator,
   lastNameValidator,
+  tokenValidator,
 } = require("@/validators/userValidators");
-
 const {
-  userErrors: { USER_NOT_EXIST, FULL_NAME_INVALID, USER_EXIST },
+  userErrors: { FULL_NAME_INVALID, USER_EXIST, USER_NOT_EXIST },
 } = require("@/variables/errors/userErrors");
+
+const tryToExtractCellphoneFromToken = async (verifyToken) => {
+  const jwtSecret = authManager.getJwtSignInSecret();
+  const verifiedToken = await tokenValidator(verifyToken, jwtSecret);
+  errorThrower(verifiedToken.ok === false, () => verifiedToken.error);
+  const cellphone = userPropsUtilities.extractCellphone(verifiedToken.payload);
+  return cellphone;
+};
+
+const tryToValidateFirstName = async (firstName) => {
+  const validatedFirstName = await firstNameValidator(firstName);
+  errorThrower(validatedFirstName.ok === false, {
+    ...FULL_NAME_INVALID,
+    validatedFullName: { validatedFirstName },
+  });
+};
+
+const tryToValidateLastName = async (lastName) => {
+  const validatedLastName = await lastNameValidator(lastName);
+  errorThrower(validatedLastName.ok === false, {
+    ...FULL_NAME_INVALID,
+    validatedFullName: { validatedLastName },
+  });
+};
+
+const tryToFindTemporaryClient = async (cellphone) => {
+  const client = await temporaryClients.findClient(cellphone);
+  errorThrower(!client, () => ({ ...USER_NOT_EXIST, cellphone }));
+  return client;
+};
+
+const tryToFindUserInDb = async (cellphone) => {
+  const foundUser = await userFinder(cellphone);
+  errorThrower(foundUser, () => USER_EXIST);
+  return foundUser;
+};
+
+const getRandomId = () =>
+  randomMaker.randomId(privateIdCommonModel.maxlength.value);
+
+const tryToSignMainToken = async (cellphone, privateId) => {
+  const mainToken = await authManager.tokenSigner({
+    ...cellphone,
+    privateId,
+  });
+  return mainToken;
+};
+
+const fixUserDataForDb = ({
+  cellphone,
+  firstName,
+  lastName,
+  mainToken,
+  privateId,
+}) => {
+  const defaultUserData = userPropsUtilities.defaultUserData();
+  const allUserData = {
+    ...defaultUserData,
+    ...cellphone,
+    firstName,
+    lastName,
+    privateId,
+    tokens: [{ mainToken }],
+  };
+  const userDataForDatabase = objectUtilities.excludeProps(allUserData, [
+    "bio",
+  ]);
+  return userDataForDatabase;
+};
+
+const tryToCreateNewUser = async (userDataForDatabase) => {
+  await createNewNormalUser(userDataForDatabase);
+};
+
+const responseToCreateNewUser = (user, res) => {
+  commonFunctionalities.controllerSuccessResponse(res, { user });
+};
+
+const catchCreateNewUser = commonFunctionalities.controllerCatchResponse;
+
+const createNewUserMultiTrier = async ({
+  firstName,
+  lastName,
+  verifyToken,
+}) => {
+  const trierInstance = trier(createNewUserMultiTrier.name, {
+    autoThrowError: true,
+  });
+
+  const cellphone = (
+    await trierInstance.tryAsync(tryToExtractCellphoneFromToken, verifyToken)
+  ).result;
+
+  await trierInstance.tryAsync(tryToValidateFirstName, firstName);
+  await trierInstance.tryAsync(tryToValidateLastName, lastName);
+  await trierInstance.tryAsync(tryToFindTemporaryClient, cellphone);
+  await trierInstance.tryAsync(tryToFindUserInDb, cellphone);
+
+  const privateId = getRandomId();
+
+  const mainToken = (
+    await trierInstance.tryAsync(tryToSignMainToken, cellphone, privateId)
+  ).result;
+
+  const userDataForDatabase = fixUserDataForDb({
+    cellphone,
+    firstName,
+    lastName,
+    mainToken,
+    privateId,
+  });
+
+  await trierInstance.tryAsync(tryToCreateNewUser, userDataForDatabase);
+
+  const userDataForSendToClient = {
+    ...cellphone,
+    firstName,
+    lastName,
+    mainToken,
+    privateId,
+  };
+
+  return userDataForSendToClient;
+};
 
 const createNewUserUserController = async (
   req = expressRequest,
   res = expressResponse
 ) => {
-  try {
-    const {
-      body: { firstName, lastName },
-    } = req;
+  const {
+    body: { firstName, lastName },
+  } = req;
+  const verifyToken = authManager.getTokenFromRequest(req);
+  const trierInstance = trier(createNewUserUserController.name);
 
-    const verifyToken = authManager.getTokenFromRequest(req);
-
-    const jwtSecret = authManager.getJwtSignInSecret();
-    const verifiedToken = await tokenValidator(verifyToken, jwtSecret);
-
-    errorThrower(verifiedToken.ok === false, () => verifiedToken.error);
-
-    const validatedFirstName = await firstNameValidator(firstName);
-    const validatedLastName = await lastNameValidator(lastName);
-
-    errorThrower(
-      [validatedFirstName, validatedLastName].every((item) => item.ok !== true),
-      () => ({
-        ...FULL_NAME_INVALID,
-        validatedFullName: { validatedFirstName, validatedLastName },
-      })
-    );
-
-    const cellphone = userPropsUtilities.extractCellphone(
-      verifiedToken.payload
-    );
-    const client = await temporaryClients.findClient(cellphone);
-    errorThrower(!client, () => ({ ...USER_NOT_EXIST, cellphone }));
-
-    const foundUser = await userFinder(cellphone);
-
-    errorThrower(foundUser, () => USER_EXIST);
-
-    const privateId = randomMaker.randomId(
-      privateIdCommonModel.maxlength.value
-    );
-
-    const mainToken = await authManager.tokenSigner({
-      ...cellphone,
-      privateId,
-    });
-
-    const defaultUserData = userPropsUtilities.defaultUserData();
-    const allUserData = {
-      ...defaultUserData,
-      ...cellphone,
+  (
+    await trierInstance.tryAsync(createNewUserMultiTrier, {
       firstName,
       lastName,
-      privateId,
-      tokens: [{ mainToken }],
-    };
-
-    const userDataForDatabase = objectUtilities.excludeProps(allUserData, [
-      "bio",
-    ]);
-    await createNewNormalUser(userDataForDatabase);
-
-    const user = { ...cellphone, privateId, firstName, lastName, mainToken };
-    res.checkDataAndResponse({ user });
-  } catch (error) {
-    logger.log("createNewUserUserController", error);
-    res.errorCollector(error);
-    res.errorResponser();
-  }
+      verifyToken,
+    })
+  )
+    .executeIfNoError(responseToCreateNewUser, res)
+    .catch(catchCreateNewUser, res);
 };
 
 module.exports = { createNewUserUserController };

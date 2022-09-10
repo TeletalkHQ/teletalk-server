2a3ed10098fe0269268526ff4f2b2e91066553f2
S-STALWART-S <stalwart.programmer@gmail.com>
2022-09-10 21:53:07 +0430

    refactored:[ testControllers with Trier ]

--- src/controllers/testControllers/getAllUsersTestController.js
+++ src/controllers/testControllers/getAllUsersTestController.js
@@ -1,18 +1,25 @@
+const { commonFunctionalities } = require("@/classes/CommonFunctionalities");
 const { getAllUsers } = require("@/models/userModels/userModelFunctions");
+const { trier } = require("utility-store/src/classes/Trier");
+
+const tryToGetAllUsers = async () => {
+  const users = await getAllUsers();
+  return users;
+};
+
+const responseToGetAllUsers = (users, res) => {
+  commonFunctionalities.controllerSuccessResponse(res, { users });
+};
+
+const catchGetAllUsers = commonFunctionalities.controllerCatchResponse;
 
 const getAllUsersTestController = async (
   _ = expressRequest,
   res = expressResponse
 ) => {
-  try {
-    const { users } = await getAllUsers();
-
-    res.checkDataAndResponse({ users });
-  } catch (error) {
-    logger.log("getAllUsersTestController", error);
-    res.errorCollector(error);
-    res.errorResponser();
-  }
+  (await trier(getAllUsersTestController.name).tryAsync(tryToGetAllUsers))
+    .executeIfNoError(responseToGetAllUsers, res)
+    .catch(catchGetAllUsers, res);
 };
 
 module.exports = { getAllUsersTestController };

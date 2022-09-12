4e583802f7f8264074790a72ff676e6aa012afbf
S-STALWART-S <stalwart.programmer@gmail.com>
2022-09-12 13:26:16 +0430

    cleanup:[ some unused UserPropsUtilities methods ]

--- src/classes/UserPropsUtilities.js
+++ src/classes/UserPropsUtilities.js
@@ -3,7 +3,6 @@ const {
 } = require("utility-store/src/classes/UserPropsUtilities");
 
 const { stateManager } = require("@/classes/StateManager");
-const { envManager } = require("@/classes/EnvironmentManager");
 
 class UserPropsUtilities extends UserPropsUtilitiesMain {
   constructor(id) {
@@ -14,22 +13,10 @@ class UserPropsUtilities extends UserPropsUtilitiesMain {
   getTokenFromUserObject(userObject) {
     return userObject.tokens[0]?.mainToken;
   }
-  getTestVerificationCode() {
-    const { TEST_VERIFICATION_CODE } = envManager.ENVIRONMENT_KEYS;
-    return envManager.getEnvironment(TEST_VERIFICATION_CODE);
-  }
-  setTestVerificationCode(verificationCode) {
-    const { TEST_VERIFICATION_CODE } = envManager.ENVIRONMENT_KEYS;
-    envManager.setEnvironment(TEST_VERIFICATION_CODE, verificationCode);
-  }
 
   async setTestUsers(testUsers) {
     const { testUsers: stateKey } = stateManager.stateKeys;
     return await stateManager.setState(stateKey, testUsers);
-  }
-  setTestVerifyToken(token) {
-    const { TEST_VERIFY_TOKEN } = envManager.ENVIRONMENT_KEYS;
-    envManager.setEnvironment(TEST_VERIFY_TOKEN, token);
   }
 }
 

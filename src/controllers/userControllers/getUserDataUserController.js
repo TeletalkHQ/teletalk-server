3d9de9cfa8f55aa63d1283198c401daba0a248df
S-STALWART-S <stalwart.programmer@gmail.com>
2022-09-12 13:27:18 +0430

    refactored:[ getUserDataUserController and singInNormalUserController with Trier ]

--- src/controllers/userControllers/getUserDataUserController.js
+++ src/controllers/userControllers/getUserDataUserController.js
@@ -1,20 +1,34 @@
+const { trier } = require("utility-store/src/classes/Trier");
+
+const { commonFunctionalities } = require("@/classes/CommonFunctionalities");
+
 const { getUserData } = require("@/services/userServices");
+
+const tryToGetUserData = async (privateId) => {
+  const user = await getUserData(privateId);
+  return user;
+};
+
+const responseToGetUserData = (user, res) => {
+  commonFunctionalities.controllerSuccessResponse(res, { user });
+};
+
+const catchGetUserData = commonFunctionalities.controllerCatchResponse;
 
 const getUserDataUserController = async (
   req = expressRequest,
   res = expressResponse
 ) => {
-  try {
-    const { privateId } = req.body;
+  const { privateId } = req.body;
 
-    const user = await getUserData(privateId);
-
-    res.checkDataAndResponse({ user });
-  } catch (error) {
-    logger.log("getUserDataUserController catch, error: ", error);
-    res.errorCollector(error);
-    res.errorResponser();
-  }
+  (
+    await trier(getUserDataUserController.name).tryAsync(
+      tryToGetUserData,
+      privateId
+    )
+  )
+    .executeIfNoError(responseToGetUserData, res)
+    .catch(catchGetUserData);
 };
 
 module.exports = { getUserDataUserController };

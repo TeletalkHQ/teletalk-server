fe4f77c2dd71f6874224f0fc332e32f8b8a50a98
S-STALWART-S <stalwart.programmer@gmail.com>
2022-09-15 23:00:10 +0430

    refactored:[ cellphoneValidatorMiddleware ]

--- src/middlewares/cellphoneValidatorMiddleware.js
+++ src/middlewares/cellphoneValidatorMiddleware.js
@@ -1,21 +1,34 @@
+const { commonFunctionalities } = require("@/classes/CommonFunctionalities");
 const { userPropsUtilities } = require("@/classes/UserPropsUtilities");
 
 const { cellphoneValidator } = require("@/validators/userValidators");
+const { trier } = require("utility-store/src/classes/Trier");
+
+const tryToValidateCellphone = async (userData) => {
+  const cellphone = userPropsUtilities.extractCellphone(userData);
+  await cellphoneValidator(cellphone);
+  return { ok: true };
+};
+
+const executeIfNoError = (_, next) => {
+  next();
+};
+
+const catchValidateCellphone = (error, res) => {
+  commonFunctionalities.controllerCatchResponse(error, res);
+  return { ok: false };
+};
 
 const cellphoneValidatorMiddleware = async (req, res, next) => {
-  try {
-    const cellphone = userPropsUtilities.extractCellphone(req.body);
-    await cellphoneValidator(cellphone);
-
-    next();
-
-    return { ok: true };
-  } catch (error) {
-    logger.log("cellphoneValidatorMiddleware catch, error:", error);
-    res.errorCollector(error);
-    res.errorResponser();
-    return { ok: false };
-  }
+  return (
+    await trier(cellphoneValidatorMiddleware.name).tryAsync(
+      tryToValidateCellphone,
+      req.body
+    )
+  )
+    .executeIfNoError(executeIfNoError, next)
+    .catch(catchValidateCellphone, res)
+    .result();
 };
 
 module.exports = { cellphoneValidatorMiddleware };

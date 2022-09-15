a0b499b3380c57341b5e7a9bb4d39032f85b17f2
S-STALWART-S <stalwart.programmer@gmail.com>
2022-09-15 23:00:58 +0430

    refactored:[ contactValidatorMiddleware ]

--- src/middlewares/contactValidatorMiddleware.js
+++ src/middlewares/contactValidatorMiddleware.js
@@ -1,30 +1,44 @@
+const { trier } = require("utility-store/src/classes/Trier");
+
+const { commonFunctionalities } = require("@/classes/CommonFunctionalities");
 const { userPropsUtilities } = require("@/classes/UserPropsUtilities");
 
 const { errorThrower } = require("@/functions/utilities/utilities");
 
 const { contactValidator } = require("@/validators/userValidators");
 
+const tryToValidateContact = async (userData) => {
+  const { firstName, lastName } = userData;
+  const validationResult = await contactValidator({
+    ...userPropsUtilities.extractCellphone(userData),
+    firstName,
+    lastName,
+  });
+  errorThrower(validationResult.ok !== true, validationResult);
+  return { ok: true };
+};
+
+const executeIfNoError = (_, next) => {
+  next();
+};
+
+const catchValidateContact = (error, res) => {
+  commonFunctionalities.controllerCatchResponse(error, res);
+  return { ok: false };
+};
+
 const contactValidatorMiddleware = async (req, res, next) => {
-  try {
-    const { firstName, lastName } = req.body;
+  const { body: userData } = req;
 
-    const validationResult = await contactValidator({
-      ...userPropsUtilities.extractCellphone(req.body),
-      firstName,
-      lastName,
-    });
-
-    errorThrower(validationResult.ok !== true, validationResult);
-
-    next();
-
-    return { ok: true };
-  } catch (error) {
-    logger.log("contactValidatorMiddleware catch, error:", error);
-    res.errorCollector(error);
-    res.errorResponser();
-    return { ok: false };
-  }
+  return (
+    await trier(contactValidatorMiddleware.name).tryAsync(
+      tryToValidateContact,
+      userData
+    )
+  )
+    .executeIfNoError(executeIfNoError, next)
+    .catch(catchValidateContact, res)
+    .result();
 };
 
 module.exports = { contactValidatorMiddleware };

05b927e7a431523541ac66362dcfd6816799a1e1
S-STALWART-S <stalwart.programmer@gmail.com>
2022-09-16 18:22:54 +0430

    refactored:[ chatValidators with Trier ]

--- src/validators/chatValidators.js
+++ src/validators/chatValidators.js
@@ -34,14 +34,7 @@ const {
     PARTICIPANT_ID_REQUIRED,
   },
 } = require("@/variables/errors/chatErrors");
-
-const checkReturnCondition = (returnCondition, error) => {
-  if (returnCondition) {
-    return error;
-  }
-
-  errorThrower(error, error);
-};
+const { trier } = require("utility-store/src/classes/Trier");
 
 const compiledChatIdValidator = ValidationModelBuilder.validatorCompiler(
   chatIdValidationModel
@@ -56,91 +49,97 @@ const compiledParticipantIdValidator = ValidationModelBuilder.validatorCompiler(
   participantIdValidationModel
 );
 
-const chatIdValidator = async (chatId, returnCondition) => {
-  try {
-    const result = await compiledChatIdValidator({ chatId });
+const tryToValidateChatId = async (chatId) => {
+  const result = await compiledChatIdValidator({ chatId });
 
-    if (result === true) return { ok: true };
+  if (result === true) return;
 
-    validationErrorBuilder
-      .create()
-      .setRequirements(result, {
-        extraErrorFields: {
-          validatedChatId: chatId,
-        },
-      })
-      .required(CHAT_ID_REQUIRED)
-      .stringEmpty(CHAT_ID_REQUIRED)
-      .string(CHAT_ID_INVALID_TYPE)
-      .stringMin(CHAT_ID_MIN_LENGTH_REACH)
-      .stringMax(CHAT_ID_MAX_LENGTH_REACH)
-      .throwAnyway(CHAT_ID_INVALID)
-      .execute();
-  } catch (error) {
-    logger.log("chatIdValidator catch, error:", error);
-    return checkReturnCondition(returnCondition, error);
-  }
+  validationErrorBuilder
+    .create()
+    .setRequirements(result, {
+      extraErrorFields: {
+        validatedChatId: chatId,
+      },
+    })
+    .required(CHAT_ID_REQUIRED)
+    .stringEmpty(CHAT_ID_REQUIRED)
+    .string(CHAT_ID_INVALID_TYPE)
+    .stringMin(CHAT_ID_MIN_LENGTH_REACH)
+    .stringMax(CHAT_ID_MAX_LENGTH_REACH)
+    .throwAnyway(CHAT_ID_INVALID)
+    .execute();
+};
+const chatIdValidator = async (chatId) => {
+  (
+    await trier(chatIdValidator).tryAsync(tryToValidateChatId, chatId)
+  ).printAndThrow();
 };
 
-const messageTextValidator = async (messageText, returnCondition) => {
-  try {
-    const result = await compiledMessageTextValidator({ message: messageText });
+const tryToValidateMessageText = async (messageText) => {
+  const result = await compiledMessageTextValidator({ message: messageText });
 
-    if (result === true) return { ok: true };
+  if (result === true) return;
 
-    validationErrorBuilder
-      .create()
-      .setRequirements(result, {
-        extraErrorFields: {
-          validatedMessageText: messageText,
-        },
-      })
-      .required(MESSAGE_TEXT_REQUIRED)
-      .stringEmpty(MESSAGE_TEXT_REQUIRED)
-      .string(MESSAGE_TEXT_INVALID_TYPE)
-      .stringMin(MESSAGE_ID_MIN_LENGTH_REACH)
-      .stringMax(MESSAGE_TEXT_MAX_LENGTH_REACH)
-      .throwAnyway(MESSAGE_TEXT_INVALID)
-      .execute();
-  } catch (error) {
-    logger.log("messageTextValidator catch, error:", error);
-    return checkReturnCondition(returnCondition, error);
-  }
+  validationErrorBuilder
+    .create()
+    .setRequirements(result, {
+      extraErrorFields: {
+        validatedMessageText: messageText,
+      },
+    })
+    .required(MESSAGE_TEXT_REQUIRED)
+    .stringEmpty(MESSAGE_TEXT_REQUIRED)
+    .string(MESSAGE_TEXT_INVALID_TYPE)
+    .stringMin(MESSAGE_ID_MIN_LENGTH_REACH)
+    .stringMax(MESSAGE_TEXT_MAX_LENGTH_REACH)
+    .throwAnyway(MESSAGE_TEXT_INVALID)
+    .execute();
+};
+const messageTextValidator = async (messageText) => {
+  (
+    await trier(messageTextValidator.name).tryAsync(
+      tryToValidateMessageText,
+      messageText
+    )
+  ).printAndThrow();
 };
 
-const participantIdValidator = async (participantId, returnCondition) => {
-  try {
-    const result = await compiledParticipantIdValidator({
-      participantId,
-    });
+const tryToValidateParticipantId = async (participantId) => {
+  const result = await compiledParticipantIdValidator({
+    participantId,
+  });
 
-    if (result === true) return { ok: true };
+  if (result === true) return;
 
-    validationErrorBuilder
-      .create()
-      .setRequirements(result, {
-        extraErrorFields: {
-          validatedParticipantId: participantId,
-        },
-      })
-      .required(PARTICIPANT_ID_REQUIRED)
-      .stringEmpty(PARTICIPANT_ID_REQUIRED)
-      .string(PARTICIPANT_ID_INVALID_TYPE)
-      .stringMin(PARTICIPANT_ID_MIN_LENGTH_REACH)
-      .stringMax(PARTICIPANT_ID_MAX_LENGTH_REACH)
-      .throwAnyway(PARTICIPANT_ID_INVALID)
-      .execute();
-  } catch (error) {
-    logger.log("privateIdValidator catch, error:", error);
-    return checkReturnCondition(returnCondition, error);
-  }
+  validationErrorBuilder
+    .create()
+    .setRequirements(result, {
+      extraErrorFields: {
+        validatedParticipantId: participantId,
+      },
+    })
+    .required(PARTICIPANT_ID_REQUIRED)
+    .stringEmpty(PARTICIPANT_ID_REQUIRED)
+    .string(PARTICIPANT_ID_INVALID_TYPE)
+    .stringMin(PARTICIPANT_ID_MIN_LENGTH_REACH)
+    .stringMax(PARTICIPANT_ID_MAX_LENGTH_REACH)
+    .throwAnyway(PARTICIPANT_ID_INVALID)
+    .execute();
+};
+const participantIdValidator = async (participantId) => {
+  (
+    await trier(participantIdValidator.name).tryAsync(
+      tryToValidateParticipantId,
+      participantId
+    )
+  ).printAndThrow();
 };
 
 const chatValidators = {
   chatIdValidator,
+  messageIdValidator,
   messageTextValidator,
   participantIdValidator,
-  messageIdValidator,
 };
 
 module.exports = { chatValidators };

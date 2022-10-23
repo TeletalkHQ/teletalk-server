8175c34d4fc4019b2dcc7769ca3d39243be337a3
S-STALWART-S <stalwart.programmer@gmail.com>
2022-10-23 12:08:20 +0330

    fixed:[ requestDetailsLogger logs args ]

--- src/middlewares/requestDetailsLogger.js
+++ src/middlewares/requestDetailsLogger.js
@@ -1,14 +1,8 @@
 const requestDetailsLogger = (req, _, next) => {
   logger
     .blue("--------------------------------------")
-    .bgBlue({
-      text: "Request arrived: ",
-      textColor: logger.colors.black,
-    })
-    .bgCyan({
-      text: `${req.url}`,
-      textColor: logger.colors.black,
-    })
+    .bgBlue("Request arrived: ", logger.colors.black)
+    .bgCyan(`${req.url}`, logger.colors.black)
     .blue("--------------------------------------")
     .info();
 

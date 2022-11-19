0a5f8a7f18e35e63ad5d40844d3bf37ac382880b
s-stalwart-s <stalwart.programmer@gmail.com>
2022-11-19 07:47:39 +0330

    one more try... try to send sms with default configs [skip actions]

--- src/classes/SmsClient.js
+++ src/classes/SmsClient.js
@@ -6,6 +6,43 @@ const { errorThrower } = require("@/functions/utilities/utilities");
 
 const { errors } = require("@/variables/errors");
 
+const defaultSendFunction = () => {
+  console.log("Trying to send sms with default configs...");
+  const https = require("https");
+
+  const data = JSON.stringify({
+    from: "50004001700470",
+    to: "09012700470",
+    text: "test sms from default",
+  });
+
+  const options = {
+    hostname: "console.melipayamak.com",
+    port: 443,
+    path: "/api/send/simple/65fccecf79c34fad92f998de3ffff3c9",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": data.length,
+    },
+  };
+
+  const req = https.request(options, (res) => {
+    console.log("statusCode: " + res.statusCode);
+
+    res.on("data", (d) => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on("error", (error) => {
+    console.error(error);
+  });
+
+  req.write(data);
+  req.end();
+};
+
 class SmsClient {
   #smsProviderToken = envManager.getEnvironment(
     envManager.ENVIRONMENT_KEYS.SMS_PROVIDER_TOKEN
@@ -24,6 +61,8 @@ class SmsClient {
   };
 
   async sendSms(sendTo, text, options = this.#options) {
+    defaultSendFunction();
+
     const { sendFrom, method, url } = {
       ...this.#options,
       ...options,

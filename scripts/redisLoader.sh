94d937c7e88a13611885d0a48906f8abe1d7ee23
s-stalwart-s <stalwart.programmer@gmail.com>
2023-01-25 19:02:36 +0330

    {   "etc": ["restructure redis loader stuff"] }

--- scripts/redisLoader.sh
+++ scripts/redisLoader.sh
@@ -1,5 +1,4 @@
 sudo apt update
-sudo apt upgrade -y
 
 sudo apt install lsb-release -y
 curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
@@ -10,7 +9,7 @@ sudo apt-get install redis -y
 
 sudo systemctl stop redis-server.service
 sudo mkdir /etc/redis/modules
-sudo cp ./temp/rejson.so /etc/redis/modules
+sudo cp ./scripts/rejson.so /etc/redis/modules
 sudo tee -a /etc/redis/redis.conf >/dev/null <<EOT
 loadmodule /etc/redis/modules/rejson.so
 EOT

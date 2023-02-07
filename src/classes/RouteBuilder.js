fd1a8ff38462acfb74ea8cb1c5c70ca37036d2df
s-stalwart-s <stalwart.programmer@gmail.com>
2023-02-07 22:48:21 +0330

    add SocketRouteBuilder

--- src/classes/RouteBuilder.js
+++ src/classes/RouteBuilder.js
@@ -1,67 +1,111 @@
-const { errors } = require("@/variables/errors");
 const { customTypeof } = require("custom-typeof");
 const { errorThrower } = require("utility-store/src/utilities/utilities");
 
+const { errors } = require("@/variables/errors");
+
 class RouteBuilder {
-  constructor(baseUrl) {
-    this.baseUrl = baseUrl;
+  constructor() {
     this.route = {
-      fullUrl: undefined,
       inputFields: {},
-      method: "get",
       outputFields: [{}],
-      statusCode: undefined,
-      url: undefined,
+      statusCode: 200,
     };
   }
 
-  #updateProperty(key, value) {
+  updateProperty(key, value) {
     this.route[key] = value;
   }
 
-  method(method) {
-    this.#updateProperty("method", method);
-    return this;
-  }
-  url(url) {
-    this.#updateProperty("url", url);
-    this.#updateProperty("fullUrl", `${this.baseUrl}${url}`);
-    return this;
-  }
-
   statusCode(statusCode) {
-    this.#updateProperty("statusCode", statusCode);
+    this.updateProperty("statusCode", statusCode);
     return this;
   }
   inputFields(inputFields = this.route.inputFields) {
-    this.#updateProperty("inputFields", inputFields);
+    this.updateProperty("inputFields", inputFields);
     return this;
   }
   outputFields(outputFields = this.route.outputFields) {
-    this.#updateProperty("outputFields", outputFields);
+    this.updateProperty("outputFields", outputFields);
     return this;
   }
 
   build() {
-    this.checkRequirements();
     return this.route;
   }
 
-  checkRequirements() {
-    const { fullUrl, url, statusCode } = this.route;
-
-    errorThrower(customTypeof.isUndefined(fullUrl, url, statusCode), {
+  checkRequirements(...requirements) {
+    errorThrower(customTypeof.isUndefined(...requirements), {
       ...errors.ROUTE_IS_BROKEN,
       route: this.route,
     });
   }
 }
 
-const routeBuilder = (baseUrl) => ({
-  create: (...args) => new RouteBuilder(baseUrl, ...args),
+class HttpRouteBuilder extends RouteBuilder {
+  constructor(baseUrl) {
+    super();
+    this.baseUrl = baseUrl;
+    this.route = {
+      fullUrl: undefined,
+      inputFields: {},
+      method: "get",
+      outputFields: [{}],
+      statusCode: 200,
+      url: undefined,
+    };
+  }
+
+  method(method) {
+    this.updateProperty("method", method);
+    return this;
+  }
+  url(url) {
+    this.updateProperty("url", url);
+    this.updateProperty("fullUrl", `${this.baseUrl}${url}`);
+    return this;
+  }
+
+  build() {
+    const { fullUrl, statusCode, url } = this.route;
+    this.checkRequirements(fullUrl, url, statusCode);
+    return this.route;
+  }
+}
+
+class SocketRouteBuilder extends RouteBuilder {
+  constructor() {
+    super();
+    this.route = {
+      event: "",
+      inputFields: {},
+      outputFields: {},
+      statusCode: 200,
+    };
+  }
+
+  event(name) {
+    this.updateProperty("event", name);
+    return this;
+  }
+
+  build() {
+    const { event, statusCode } = this.route;
+    this.checkRequirements(event, statusCode);
+    return this.route;
+  }
+}
+
+const socketRouteBuilder = () => ({
+  create: () => new SocketRouteBuilder(),
+});
+
+const httpRouteBuilder = (baseUrl) => ({
+  create: (...args) => new HttpRouteBuilder(baseUrl, ...args),
 });
 
 module.exports = {
-  routeBuilder,
-  RouteBuilder,
+  httpRouteBuilder,
+  HttpRouteBuilder,
+  socketRouteBuilder,
+  SocketRouteBuilder,
 };

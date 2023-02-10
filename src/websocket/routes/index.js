b55f35c52a95dba4722b071dedce92f9c73b4b22
s-stalwart-s <stalwart.programmer@gmail.com>
2023-02-10 20:53:56 +0330

    add user routes

--- src/websocket/routes/index.js
+++ src/websocket/routes/index.js
@@ -1,9 +1,11 @@
 const { authRoutes } = require("@/websocket/routes/auth");
 const { otherRoutes } = require("@/websocket/routes/other");
+const { userRoutes } = require("@/websocket/routes/user");
 
 const routes = {
   auth: authRoutes,
   other: otherRoutes,
+  user: userRoutes,
 };
 
 module.exports = {

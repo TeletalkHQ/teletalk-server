58431030abbf8b1bb9aa0842f8c7802fc055a136
s-stalwart-s <stalwart.programmer@gmail.com>
2023-02-13 23:16:50 +0330

    add websocket new custom: emit

---
+++ src/websocket/custom/methods/emit.js
@@ -0,0 +1,40 @@
+const { errorThrower } = require("utility-store/src/utilities/utilities");
+const {
+  ioFieldsChecker,
+} = require("utility-store/src/utilities/ioFieldsChecker");
+const { trier } = require("utility-store/src/classes/Trier");
+
+const { errors } = require("@/variables/errors");
+
+const { arrayOfRoutes } = require("@/websocket/events");
+
+const customEmit = (socket = socketIntellisense) => {
+  return (event, data, ...args) => {
+    const foundRoute = arrayOfRoutes.find((item) => item.name === event);
+
+    return trier(customEmit.name)
+      .try(tryBlock, data || {}, foundRoute.outputFields)
+      .executeIfNoError(executeIfNoError, socket, event, data, ...args)
+      .catch(catchBlock)
+      .run();
+  };
+};
+
+const tryBlock = (data, inputFields) => {
+  const checkResult = ioFieldsChecker(data, inputFields, errors.io.output);
+
+  errorThrower(checkResult.ok === false, () => ({
+    ...checkResult.error,
+    inputFields,
+  }));
+};
+
+const executeIfNoError = (_, socket, event, ...args) => {
+  socket.emit(event, ...args);
+};
+
+const catchBlock = (error, socket) => {
+  socket.emit("error", error);
+};
+
+module.exports = { customEmit };

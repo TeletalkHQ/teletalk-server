009e64ecaba416cc1d15ac43c0c2b2e03562792b
s-stalwart-s <stalwart.programmer@gmail.com>
2023-02-20 23:30:05 +0330

    refactor signIn http request to websocket event

--- src/http/routers/auth.js
+++ src/http/routers/auth.js
@@ -8,11 +8,11 @@ const { routes } = require("@/http/routes");
 
 const authRouter = Router();
 
-authRouter[routes.auth.signIn.method](
-  routes.auth.signIn.url,
-  middlewares.cellphoneValidator,
-  controllers.signIn
-);
+// authRouter[routes.auth.signIn.method](
+//   routes.auth.signIn.url,
+//   middlewares.cellphoneValidator,
+//   controllers.signIn
+// );
 
 authRouter[routes.auth.verify.method](
   routes.auth.verify.url,
--- src/websocket/events/auth/handlers/index.js
+++ src/websocket/events/auth/handlers/index.js
@@ -2,10 +2,12 @@ const {
   createNewUser,
 } = require("@/websocket/events/auth/handlers/createNewUser");
 const { logout } = require("@/websocket/events/auth/handlers/logout");
+const { signIn } = require("@/websocket/events/auth/handlers/signIn");
 
 const authHandlers = {
   createNewUser,
   logout,
+  signIn,
 };
 
 module.exports = {

5950e04fc5c8e20cc67424db013a5c626262e765
s-stalwart-s <stalwart.programmer@gmail.com>
2023-02-20 23:45:07 +0330

    refactor getWelcomeMessage http request to websocket event

--- src/http/controllers/index.js
+++ src/http/controllers/index.js
@@ -1,12 +1,7 @@
-const {
-  getWelcomeMessage,
-} = require("@/http/controllers/other/getWelcomeMessage");
-
 const { getAllStuff } = require("@/http/controllers/stuff/getAllStuff");
 
 const controllers = {
   getAllStuff,
-  getWelcomeMessage,
 };
 
 module.exports = { controllers };
--- src/websocket/events/other/handlers/index.js
+++ src/websocket/events/other/handlers/index.js
@@ -1,11 +1,15 @@
 const {
   getCountries,
 } = require("@/websocket/events/other/handlers/getCountries");
+const {
+  getWelcomeMessage,
+} = require("@/websocket/events/other/handlers/getWelcomeMessage");
 const { logEvent } = require("@/websocket/events/other/handlers/logEvent");
 const { ping } = require("@/websocket/events/other/handlers/ping");
 
 const otherHandlers = {
   getCountries,
+  getWelcomeMessage,
   logEvent,
   ping,
 };

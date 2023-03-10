332263e7c9640ae65e036eb4d5360ffa50370783
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-10 13:52:48 +0330

    fix findOneUser service

--- src/services/common/findOneUser.ts
+++ src/services/common/findOneUser.ts
@@ -1,8 +1,14 @@
 import { models } from "@/models";
+import { UserMongo } from "@/types";
+import { ProjectionType, QueryOptions } from "mongoose";
 
 const User = models.database.mongoDb.User;
 
-const findOneUser = async (userData, projection, options) => {
+const findOneUser = async (
+  userData: Partial<UserMongo>,
+  projection: ProjectionType<UserMongo>,
+  options: QueryOptions
+) => {
   return await User.findOne(userData, projection, options);
 };
 

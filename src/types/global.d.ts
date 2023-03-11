0da74f93c5fa7ed2bc6b11123a347b05c921fa02
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-11 18:54:44 +0330

    fix websocket server

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -3,10 +3,21 @@ import { LoggerChalker } from "logger-chalker";
 
 declare global {
   var logger: LoggerChalker;
-
-  var Config: {
-    Foo: string;
-  };
-  var Foo: string;
 }
 export {};
+
+declare module "socket.io" {
+  interface Socket {
+    customEmit(event: string, data: any): void;
+
+    customOn(
+      event: string,
+      callback: (...args: any[]) => Promise<void | object>
+    ): void;
+
+    customUse(
+      middleware: (...args: any[]) => Promise<void> | void,
+      ...args: any[]
+    ): void;
+  }
+}

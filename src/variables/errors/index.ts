2aa0bbeacebf22b2844c7b5c62777e7234984982
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-11 19:24:24 +0330

    fix io errors

--- src/variables/errors/index.ts
+++ src/variables/errors/index.ts
@@ -2,20 +2,26 @@ import { localErrors } from "@/variables/errors/local";
 import { serverErrors } from "@/variables/errors/server";
 import { userErrors } from "@/variables/errors/user";
 
+const schemaErrors = {
+  schemaInvalid: serverErrors.REQUIRED_FIELD_INVALID,
+  schemaInvalidType: serverErrors.REQUIRED_FIELD_INVALID_TYPE,
+  schemaNotDefined: serverErrors.REQUIRED_FIELDS_NOT_DEFINED,
+};
+
 const ioErrors = {
   input: {
-    ioDataFieldInvalidType: userErrors.INPUT_FIELD_INVALID_TYPE,
-    missingFieldsError: userErrors.INPUT_FIELDS_MISSING,
-    overloadFieldsError: userErrors.INPUT_FIELDS_OVERLOAD,
-    requiredFieldsNotDefinedError: serverErrors.REQUIRED_FIELDS_NOT_DEFINED,
-    requiredFieldInvalidType: serverErrors.REQUIRED_FIELD_INVALID_TYPE,
+    ...schemaErrors,
+    dataFieldInvalidType: userErrors.INPUT_FIELD_INVALID_TYPE,
+    dataFieldsMissing: userErrors.INPUT_FIELDS_MISSING,
+    dataFieldsOverload: userErrors.INPUT_FIELDS_OVERLOAD,
+    dataNotDefined: userErrors.INPUT_DATA_NOT_DEFINED,
   },
   output: {
-    ioDataFieldTypeWrongError: serverErrors.OUTPUT_FIELD_TYPE_WRONG,
-    missingFieldsError: serverErrors.OUTPUT_FIELDS_MISSING,
-    overloadFieldsError: serverErrors.OUTPUT_FIELDS_OVERLOAD,
-    requiredFieldsNotDefinedError: serverErrors.REQUIRED_FIELDS_NOT_DEFINED,
-    requiredFieldInvalidType: serverErrors.REQUIRED_FIELD_INVALID_TYPE,
+    ...schemaErrors,
+    dataFieldInvalidType: serverErrors.OUTPUT_FIELD_INVALID_TYPE,
+    dataFieldsMissing: serverErrors.OUTPUT_FIELDS_MISSING,
+    dataFieldsOverload: serverErrors.OUTPUT_FIELDS_OVERLOAD,
+    dataNotDefined: userErrors.OUTPUT_DATA_NOT_DEFINED,
   },
 };
 

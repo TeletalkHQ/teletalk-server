c6ba1251b9a4d4761fabb65e815d6b0aaa8f0696
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-13 01:51:51 +0330

    fix middleware: checkEventAvailability

--- src/websocket/middlewares/checkEventAvailability.ts
+++ src/websocket/middlewares/checkEventAvailability.ts
@@ -1,28 +1,19 @@
-import { customTypeof } from "custom-typeof";
+import { SocketMiddleware } from "@/types";
 
 import { errors } from "@/variables/errors";
 
 import { arrayOfRoutes } from "@/websocket/events";
-import { errorThrower } from "utility-store";
 
-const checkEventAvailability = (_socket, next, [name]) => {
+const checkEventAvailability: SocketMiddleware = (_socket, next, [name]) => {
   const foundEvent = arrayOfRoutes.find((item) => item.name === name);
 
-  errorThrower(isEventInvalid(foundEvent), {
-    ...errors.EVENT_NOT_FOUND,
-    event: name,
-  });
+  if (!foundEvent)
+    throw {
+      ...errors.EVENT_NOT_FOUND,
+      event: name,
+    };
 
   next();
 };
-
-const isEventInvalid = ({
-  inputFields,
-  method,
-  name,
-  outputFields,
-  statusCode,
-} = {}) =>
-  customTypeof.isUndefined(inputFields, method, name, outputFields, statusCode);
 
 export { checkEventAvailability };

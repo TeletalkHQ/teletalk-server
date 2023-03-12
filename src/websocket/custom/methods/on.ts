1fc50fb8046ffad6fd6b30e975fb8389f15b1fcf
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-13 01:15:19 +0330

    fix socket customOn

--- src/websocket/custom/methods/on.ts
+++ src/websocket/custom/methods/on.ts
@@ -1,23 +1,23 @@
 import { Socket } from "socket.io";
 
-const customOn =
-  //prettier-ignore
-  (socket: Socket) =>
-    (event: string, callback: (...args:unknown[]) => Promise<void | object>) => {
-      socket.on(event, async (...args: unknown[]) => {
-        try {
-          const returnValue = await callback(...args);
+import { ClientCallback, CustomOn, StringMap } from "@/types";
 
-          const cb = args[1];
-          if (returnValue) {
-            if (cb) cb(returnValue);
-            else socket.customEmit(event, returnValue);
-          }
-        } catch (error) {
-          logger.log(logger.levels.debug, "socket.customOn.error:", error);
-          socket.emit("error", error);
+const customOn = (socket: Socket) => {
+  return ((event, callback) => {
+    socket.on(event, async (data: StringMap, cb: ClientCallback) => {
+      try {
+        const returnValue = await callback(socket, data);
+
+        if (returnValue) {
+          if (cb) cb(returnValue);
+          else socket.customEmit(event, returnValue);
         }
-      });
-    };
+      } catch (error) {
+        logger.log(logger.levels.debug, "socket.customOn.error:", error);
+        socket.emit("error", error);
+      }
+    });
+  }) as CustomOn;
+};
 
 export { customOn };

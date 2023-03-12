4f043838e7d57f3929e33548783841f289ba2450
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-13 02:02:27 +0330

    fix middleware: auth

--- src/websocket/middlewares/auth.ts
+++ src/websocket/middlewares/auth.ts
@@ -1,34 +1,38 @@
 import { trier } from "simple-trier";
+import { Socket } from "socket.io";
 
 import { authManager } from "@/classes/AuthManager";
+
+import { SocketMiddleware, SocketNext, VerifiedToken } from "@/types";
 
 import { validators } from "@/validators";
 
 import { errors } from "@/variables/errors";
 
-const auth = async (socket, next) => {
-  await trier(auth.name)
+const auth: SocketMiddleware = async (socket, next) => {
+  await trier<VerifiedToken>(auth.name)
     .tryAsync(tryBlock, socket)
     .executeIfNoError(executeIfNoError, socket, next)
     .throw()
     .runAsync();
 };
 
-const tryBlock = async (socket) => {
+const tryBlock = async (socket: Socket) => {
   if (!socket.handshake.headers.cookie) throw errors.TOKEN_REQUIRED;
 
   const token = authManager.getTokenFromSocket(socket);
 
-  const validationResult = await validators.token(
-    token,
-    authManager.getMainSecret()
-  );
+  if (!token) throw errors.TOKEN_REQUIRED;
 
-  return { validationResult };
+  return await validators.token(token, authManager.getMainSecret());
 };
 
-const executeIfNoError = ({ validationResult }, socket, next) => {
-  socket.authData = validationResult;
+const executeIfNoError = (
+  verifiedToken: VerifiedToken,
+  socket: Socket,
+  next: SocketNext
+) => {
+  socket.authData = verifiedToken;
   next();
 };
 

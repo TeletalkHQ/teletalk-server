2825636d71ff2c12a2d9cd4db393abf1d15841a9
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-13 16:17:27 +0330

    fix handler: sendPrivateMessage

--- src/websocket/events/privateChat/handlers/sendPrivateMessage.ts
+++ src/websocket/events/privateChat/handlers/sendPrivateMessage.ts
@@ -1,19 +1,24 @@
 import { services } from "@/services";
 
+import { SocketOnHandler } from "@/types";
+
 import { validators } from "@/validators";
 
-const sendPrivateMessage = async (socket, io, data) => {
+const sendPrivateMessage: SocketOnHandler = async (socket, data) => {
   const { currentUserId } = socket;
   const { participantId, message } = data;
 
   await validators.participantId(participantId);
   await validators.messageText(message);
 
-  const { chatId, newMessage } = await await services
-    .sendPrivateMessage()
-    .run({ currentUserId, participantId, message });
+  const { chatId, newMessage } = await services.sendPrivateMessage({
+    currentUserId,
+    participantId,
+    message,
+  });
 
-  io.to(currentUserId)
+  socket.io
+    .to(currentUserId)
     .to(participantId)
     //TODO: Use customEmit
     .emit("newPrivateChatMessage", { chatId, newMessage });

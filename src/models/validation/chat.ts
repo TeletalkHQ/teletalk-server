eec39f86f6743b66a54980079cf09611e830f210
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-14 23:04:20 +0330

    fix chat validation models

--- src/models/validation/chat.ts
+++ src/models/validation/chat.ts
@@ -2,52 +2,60 @@ import { validationModelBuilder } from "@/classes/modelBuilder/ValidationModelBu
 
 import { nativeModels } from "@/models/native";
 
-const messageId = validationModelBuilder
-  .create()
-  .setModel(nativeModels.chat.messageId)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .unique()
-  .trim()
-  .build();
+const messageId = {
+  messageId: validationModelBuilder
+    .create()
+    .setModel(nativeModels.chat.messageId)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .unique()
+    .trim()
+    .build(),
+};
 
-const chatId = validationModelBuilder
-  .create()
-  .setModel(nativeModels.chat.chatId)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .unique()
-  .trim()
-  .build();
+const chatId = {
+  chatId: validationModelBuilder
+    .create()
+    .setModel(nativeModels.chat.chatId)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .unique()
+    .trim()
+    .build(),
+};
 
-const messageText = validationModelBuilder
-  .create()
-  .setModel(nativeModels.chat.message)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .trim()
-  .build();
+const messageText = {
+  messageText: validationModelBuilder
+    .create()
+    .setModel(nativeModels.chat.message)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .trim()
+    .build(),
+};
 
-const participantId = validationModelBuilder
-  .create()
-  .setModel(nativeModels.chat.participantId)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .trim()
-  .unique()
-  .build();
+const participantId = {
+  participantId: validationModelBuilder
+    .create()
+    .setModel(nativeModels.chat.participantId)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .trim()
+    .unique()
+    .build(),
+};
 
 const chatValidationModels = {
   chatId,

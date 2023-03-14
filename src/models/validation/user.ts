89092e5019f4c4d38087dfc8168499fc68a507c8
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-14 23:04:28 +0330

    fix user validation models

--- src/models/validation/user.ts
+++ src/models/validation/user.ts
@@ -2,131 +2,153 @@ import { validationModelBuilder } from "@/classes/modelBuilder/ValidationModelBu
 
 import { nativeModels } from "@/models/native";
 
-const bio = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.bio)
-  .type()
-  .required()
-  .max()
-  .build();
-
-const countryCode = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.countryCode)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .numeric()
-  .trim()
-  .build();
-
-const countryName = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.countryName)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .trim()
-  .build();
-
-const firstName = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.firstName)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .trim()
-  .build();
-
-const lastName = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.lastName)
-  .type()
-  .required()
-  .min()
-  .max()
-  .trim()
-  .build();
-
-const macAddress = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.macAddress)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .trim()
-  .unique()
-  .build();
-
-const phoneNumber = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.phoneNumber)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .numeric()
-  .build();
-
-const cellphone = {
-  countryCode,
-  countryName,
-  phoneNumber,
+const bio = {
+  bio: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.bio)
+    .type()
+    .required()
+    .max()
+    .build(),
 };
 
-const userId = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.userId)
-  .required()
-  .type()
-  .empty()
-  .min()
-  .max()
-  .trim()
-  .unique()
-  .build();
+const countryCode = {
+  countryCode: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.countryCode)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .numeric()
+    .trim()
+    .build(),
+};
 
-const token = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.token)
-  .type()
-  .required()
-  .empty()
-  .min()
-  .max()
-  .build();
+const countryName = {
+  countryName: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.countryName)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .trim()
+    .build(),
+};
 
-const verificationCode = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.verificationCode)
-  .required()
-  .empty()
-  .length()
-  // .max()
-  .numeric()
-  .trim()
-  .type()
-  .build();
+const firstName = {
+  firstName: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.firstName)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .trim()
+    .build(),
+};
 
-const username = validationModelBuilder
-  .create()
-  .setModel(nativeModels.user.username)
-  .type()
-  .required()
-  .min()
-  .max()
-  .trim()
-  .unique()
-  .build();
+const lastName = {
+  lastName: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.lastName)
+    .type()
+    .required()
+    .min()
+    .max()
+    .trim()
+    .build(),
+};
+
+const macAddress = {
+  macAddress: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.macAddress)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .trim()
+    .unique()
+    .build(),
+};
+
+const phoneNumber = {
+  phoneNumber: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.phoneNumber)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .numeric()
+    .build(),
+};
+
+const cellphone = {
+  ...countryCode,
+  ...countryName,
+  ...phoneNumber,
+};
+
+const userId = {
+  userId: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.userId)
+    .required()
+    .type()
+    .empty()
+    .min()
+    .max()
+    .trim()
+    .unique()
+    .build(),
+};
+
+const token = {
+  token: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.token)
+    .type()
+    .required()
+    .empty()
+    .min()
+    .max()
+    .build(),
+};
+
+const verificationCode = {
+  verificationCode: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.verificationCode)
+    .required()
+    .empty()
+    .length()
+    // .max()
+    .numeric()
+    .trim()
+    .type()
+    .build(),
+};
+
+const username = {
+  username: validationModelBuilder
+    .create()
+    .setModel(nativeModels.user.username)
+    .type()
+    .required()
+    .min()
+    .max()
+    .trim()
+    .unique()
+    .build(),
+};
 
 const userValidationModels = {
   bio,

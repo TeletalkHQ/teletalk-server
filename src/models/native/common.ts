704666b4bea2bf06de690e104dbb5d3f7415eaac
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-15 19:29:51 +0330

    refactor model types

--- src/models/native/common.ts
+++ src/models/native/common.ts
@@ -1,18 +1,17 @@
 import { nativeModelBuilder } from "@/classes/modelBuilder/NativeModelBuilder";
 
 import { errors } from "@/variables/errors";
-import { FIELD_TYPE } from "@/variables/others/fieldType";
 
 const createdAt = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.NUMBER, errors.CREATED_AT_INVALID_TYPE)
+  .type("number", errors.CREATED_AT_INVALID_TYPE)
   .required(true, errors.CREATED_AT_REQUIRED)
   .empty(false, errors.CREATED_AT_EMPTY)
   .build();
 
 const userId = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.USER_ID_INVALID_TYPE)
+  .type("string", errors.USER_ID_INVALID_TYPE)
   .required(true, errors.USER_ID_REQUIRED)
   .empty(false, errors.USER_ID_EMPTY)
   .minlength(30, errors.USER_ID_MIN_LENGTH_REACH)
@@ -23,7 +22,7 @@ const userId = nativeModelBuilder
 
 const chatId = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.CHAT_ID_INVALID_TYPE)
+  .type("string", errors.CHAT_ID_INVALID_TYPE)
   .required(true, errors.CHAT_ID_REQUIRED)
   .empty(false, errors.CHAT_ID_EMPTY)
   .minlength(30, errors.CHAT_ID_MIN_LENGTH_REACH)
@@ -34,7 +33,7 @@ const chatId = nativeModelBuilder
 
 const messageId = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.MESSAGE_ID_INVALID_TYPE)
+  .type("string", errors.MESSAGE_ID_INVALID_TYPE)
   .required(true, errors.MESSAGE_ID_REQUIRED)
   .empty(false, errors.MESSAGE_ID_EMPTY)
   .maxlength(45, errors.MESSAGE_ID_MAX_LENGTH_REACH)
--- src/models/native/user.ts
+++ src/models/native/user.ts
@@ -3,11 +3,10 @@ import { nativeModelBuilder } from "@/classes/modelBuilder/NativeModelBuilder";
 import { commonModels } from "@/models/native/common";
 
 import { errors } from "@/variables/errors";
-import { FIELD_TYPE } from "@/variables/others/fieldType";
 
 const bio = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.BIO_INVALID_TYPE)
+  .type("string", errors.BIO_INVALID_TYPE)
   .required(true, errors.BIO_REQUIRED)
   .empty(true)
   .trim(true)
@@ -18,7 +17,7 @@ const bio = nativeModelBuilder
 
 const countryCode = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.COUNTRY_CODE_INVALID_TYPE)
+  .type("string", errors.COUNTRY_CODE_INVALID_TYPE)
   .required(true, errors.COUNTRY_CODE_REQUIRED)
   .empty(false, errors.COUNTRY_CODE_EMPTY)
   .minlength(1, errors.COUNTRY_CODE_MINLENGTH_REACH)
@@ -29,7 +28,7 @@ const countryCode = nativeModelBuilder
 
 const countryName = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.COUNTRY_NAME_INVALID_TYPE)
+  .type("string", errors.COUNTRY_NAME_INVALID_TYPE)
   .required(true, errors.COUNTRY_NAME_REQUIRED)
   .empty(false, errors.COUNTRY_NAME_EMPTY)
   .minlength(2, errors.COUNTRY_NAME_MINLENGTH_REACH)
@@ -43,7 +42,7 @@ const userId = commonModels.userId;
 
 const firstName = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.FIRST_NAME_INVALID_TYPE)
+  .type("string", errors.FIRST_NAME_INVALID_TYPE)
   .required(true, errors.FIRST_NAME_REQUIRED)
   .empty(false, errors.FIRST_NAME_EMPTY)
   .minlength(2, errors.FIRST_NAME_MINLENGTH_REACH)
@@ -53,7 +52,7 @@ const firstName = nativeModelBuilder
 
 const lastName = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.LAST_NAME_INVALID_TYPE)
+  .type("string", errors.LAST_NAME_INVALID_TYPE)
   .required(true, errors.LAST_NAME_REQUIRED)
   .empty(true)
   .minlength(2, errors.LAST_NAME_MINLENGTH_REACH)
@@ -63,7 +62,7 @@ const lastName = nativeModelBuilder
 
 const macAddress = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.MAC_ADDRESS_INVALID_TYPE)
+  .type("string", errors.MAC_ADDRESS_INVALID_TYPE)
   .required(true, errors.MAC_ADDRESS_REQUIRED)
   .empty(false, errors.MAC_ADDRESS_EMPTY)
   .minlength(12, errors.MAC_ADDRESS_MINLENGTH_REACH)
@@ -74,14 +73,14 @@ const macAddress = nativeModelBuilder
 
 const online = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.BOOLEAN, errors.ONLINE_INVALID_TYPE)
+  .type("boolean", errors.ONLINE_INVALID_TYPE)
   .required(true, errors.ONLINE_REQUIRED)
   .defaultValue(false)
   .build();
 
 const phoneNumber = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.PHONE_NUMBER_INVALID_TYPE)
+  .type("string", errors.PHONE_NUMBER_INVALID_TYPE)
   .required(true, errors.PHONE_NUMBER_REQUIRED)
   .empty(false, errors.PHONE_NUMBER_EMPTY)
   .minlength(10, errors.PHONE_NUMBER_MINLENGTH_REACH)
@@ -92,14 +91,14 @@ const phoneNumber = nativeModelBuilder
 
 const status = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.OBJECT, errors.STATUS_INVALID_TYPE)
+  .type("object", errors.STATUS_INVALID_TYPE)
   .required(true, errors.STATUS_REQUIRED)
   .defaultValue({})
   .build();
 
 const token = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.TOKEN_INVALID_TYPE)
+  .type("string", errors.TOKEN_INVALID_TYPE)
   .required(true, errors.TOKEN_REQUIRED)
   .empty(false, errors.TOKEN_EMPTY)
   .minlength(100, errors.TOKEN_MINLENGTH_REACH)
@@ -109,7 +108,7 @@ const token = nativeModelBuilder
 
 const username = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.USERNAME_INVALID_TYPE)
+  .type("string", errors.USERNAME_INVALID_TYPE)
   .required(true, errors.USERNAME_REQUIRED)
   .empty(true)
   .minlength(0, errors.USERNAME_MINLENGTH_REACH)
@@ -120,7 +119,7 @@ const username = nativeModelBuilder
 
 const verificationCode = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.STRING, errors.VERIFICATION_CODE_INVALID_TYPE)
+  .type("string", errors.VERIFICATION_CODE_INVALID_TYPE)
   .required(true, errors.VERIFICATION_CODE_REQUIRED)
   .empty(false, errors.VERIFICATION_CODE_EMPTY)
   .length(6, errors.VERIFICATION_CODE_INVALID_LENGTH)
@@ -130,21 +129,21 @@ const verificationCode = nativeModelBuilder
 
 const blacklist = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.ARRAY, errors.BLACKLIST_INVALID_TYPE)
+  .type("array", errors.BLACKLIST_INVALID_TYPE)
   .required(true, errors.BLACKLIST_REQUIRED)
   .empty(true)
   .build();
 
 const contacts = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.ARRAY, errors.CONTACTS_INVALID_TYPE)
+  .type("array", errors.CONTACTS_INVALID_TYPE)
   .required(true, errors.CONTACTS_REQUIRED)
   .empty(true)
   .build();
 
 const sessions = nativeModelBuilder
   .create()
-  .type(FIELD_TYPE.ARRAY, errors.SESSIONS_INVALID_TYPE)
+  .type("array", errors.SESSIONS_INVALID_TYPE)
   .required(true, errors.SESSIONS_REQUIRED)
   .empty(true)
   .build();

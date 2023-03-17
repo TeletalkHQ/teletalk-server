06c3e8996df89a1d2809b66e555c650fb67aa7f5
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-17 05:26:26 +0330

    fix addBlock handler [skip actions]

--- src/websocket/events/user/handlers/addBlock.ts
+++ src/websocket/events/user/handlers/addBlock.ts
@@ -1,19 +1,16 @@
-import { userUtilities } from "@/classes/UserUtilities";
-
 import { services } from "@/services";
 
-import { Cellphone, SocketOnHandler } from "@/types";
+import { SocketOnHandler } from "@/types";
 
 const addBlock: SocketOnHandler = async (socket, data) => {
   const { currentUserId } = socket;
-  const blockingCellphone = userUtilities.extractCellphone(data as Cellphone);
 
   await services.addBlock({
-    blockingUserId: blockingCellphone,
+    blockingUserId: data.userId,
     currentUserId,
   });
 
-  return { blockedCellphone: blockingCellphone };
+  return { blockedCellphone: data };
 };
 
 export { addBlock };

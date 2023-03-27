afc1d477eb9b73f10d4ba88c838e49f671439f8a
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 02:55:33 +0330

    add hander: getPrivateChat

--- src/websocket/events/privateChat/handlers/index.ts
+++ src/websocket/events/privateChat/handlers/index.ts
@@ -1,12 +1,12 @@
 import { getChatInfo } from "@/websocket/events/privateChat/handlers/getChatInfo";
-// import { getPrivateChat } from "@/websocket/events/privateChat/handlers/getPrivateChat";
+import { getPrivateChat } from "@/websocket/events/privateChat/handlers/getPrivateChat";
 import { getPrivateChats } from "@/websocket/events/privateChat/handlers/getPrivateChats";
 import { joinRoom } from "@/websocket/events/privateChat/handlers/joinRoom";
 import { sendPrivateMessage } from "@/websocket/events/privateChat/handlers/sendPrivateMessage";
 
 const privateChatHandlers = {
   getChatInfo,
-  // getPrivateChat,
+  getPrivateChat,
   getPrivateChats,
   joinRoom,
   sendPrivateMessage,

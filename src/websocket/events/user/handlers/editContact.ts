77f05e45ebcad691f3828b5a409b51f90bdcdaf9
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-27 08:29:31 +0330

    fix handler: editContact

--- src/websocket/events/user/handlers/editContact.ts
+++ src/websocket/events/user/handlers/editContact.ts
@@ -3,15 +3,22 @@ import { userUtilities } from "@/classes/UserUtilities";
 import { services } from "@/services";
 
 import { Contact, SocketOnHandler } from "@/types";
+import { validators } from "@/validators";
 
 const editContact: SocketOnHandler = async (socket, data) => {
   const { currentUserId } = socket;
   const editValues = userUtilities.extractContact(data as Contact);
 
+  await validators.userId(editValues.userId);
+  await validators.firstName(editValues.firstName);
+  await validators.lastName(editValues.lastName);
+
   await services.updateContact({ currentUserId, editValues });
 
   return {
-    editedContact: { ...editValues, userId: data.userId },
+    data: {
+      editedContact: { ...editValues, userId: data.userId },
+    },
   };
 };
 

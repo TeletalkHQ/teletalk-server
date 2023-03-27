1298a86e4aaabef2b52a3d2944ce7263987750f1
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 03:09:16 +0330

    fix validators params

--- src/validators/chat.ts
+++ src/validators/chat.ts
@@ -2,19 +2,21 @@ import { compiledValidators } from "@/helpers/compiledValidators";
 import { validationChecker } from "@/helpers/validationChecker";
 
 const chatIdValidator = async (chatId: unknown) => {
-  const validationResult = await compiledValidators.chatId(chatId);
+  const validationResult = await compiledValidators.chatId({ chatId });
   validationChecker.chatId(validationResult, chatId);
 };
 
 const messageTextValidator = async (messageText: unknown) => {
-  const validationResult = await compiledValidators.messageText(messageText);
+  const validationResult = await compiledValidators.messageText({
+    messageText,
+  });
   validationChecker.message(validationResult, messageText);
 };
 
 const participantIdValidator = async (participantId: unknown) => {
-  const validationResult = await compiledValidators.participantId(
-    participantId
-  );
+  const validationResult = await compiledValidators.participantId({
+    participantId,
+  });
   validationChecker.participantId(validationResult, participantId);
 };
 

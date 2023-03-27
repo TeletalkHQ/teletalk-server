f7abbb5976f26c3d809feb7f77dd4f0bed4c9bac
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 02:55:33 +0330

    add hander: getPrivateChat

---
+++ src/websocket/events/privateChat/handlers/getPrivateChat.ts
@@ -0,0 +1,15 @@
+import { services } from "@/services";
+
+import { SocketOnHandler } from "@/types";
+
+const getPrivateChat: SocketOnHandler = async (_socket, data) => {
+  const privateChat = await services.findOnePrivateChatByChatId(
+    { chatId: data.chatId },
+    undefined,
+    { lean: true }
+  );
+
+  return { data: { privateChat } };
+};
+
+export { getPrivateChat };

7242b01204fe9dc0f5610cc3a201969c15229644
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 03:55:06 +0330

    refactor countryCode fail helper

--- __tests__/helpers/fail/countryCode.ts
+++ __tests__/helpers/fail/countryCode.ts
@@ -1,26 +1,29 @@
 import { failTestBuilder } from "$/classes/FailTestBuilder";
 import { randomMaker } from "$/classes/RandomMaker";
 
-import { getWrongCountryCode } from "$/utilities";
-
 import { models } from "@/models";
 
-const userModels = models.native.user;
+import { FailTestExecutor } from "$/types";
+
+import { utilities } from "$/utilities";
 
 import { errors } from "@/variables/errors";
 
-const countryCodeFailTest = (configuredRequester, data) => {
+const userModels = models.native.user;
+
+const countryCodeFailTest: FailTestExecutor = (configuredRequester, data) => {
   failTestBuilder
     .create(configuredRequester, data, userModels.countryCode, "countryCode")
     .missing()
     .overload()
     .invalidType()
-    .empty.numeric()
+    .empty()
+    .numeric()
     .minlength()
     .maxlength(
       randomMaker.stringNumber(userModels.countryCode.maxlength.value + 1)
     )
-    .custom(getWrongCountryCode(), errors.COUNTRY_CODE_NOT_SUPPORTED);
+    .custom(utilities.getWrongCountryCode(), errors.COUNTRY_CODE_NOT_SUPPORTED);
 };
 
 export { countryCodeFailTest };

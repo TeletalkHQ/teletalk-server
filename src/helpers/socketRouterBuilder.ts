d733e28cdf7717d5f71574a51de78c5ae803f465
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 03:43:40 +0330

    random fix&cleanup

--- src/helpers/socketRouterBuilder.ts
+++ src/helpers/socketRouterBuilder.ts
@@ -2,13 +2,13 @@ import { Socket } from "socket.io";
 
 import { SocketOnHandler, SocketRoute } from "@/types";
 
-type Routes = {
+interface Routes {
   [prop: string]: SocketRoute;
-};
+}
 
 const socketRouterBuilder = (routes: Routes) => (socket: Socket) =>
   Object.values(routes).forEach((item) => {
-    socket["customOn"](item.name, item.handler as SocketOnHandler);
+    socket.customOn(item.name, item.handler as SocketOnHandler);
   });
 
 export { socketRouterBuilder };

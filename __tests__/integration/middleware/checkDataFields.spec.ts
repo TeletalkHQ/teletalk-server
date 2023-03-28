a733ffdabf7fea394f0c8edb0ec8f4a308b625a8
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 03:53:35 +0330

    refactor checkDataFields.spec

--- __tests__/integration/middleware/checkDataFields.spec.ts
+++ __tests__/integration/middleware/checkDataFields.spec.ts
@@ -1,15 +1,19 @@
-import { failTestBuilder } from "$/classes/FailTestBuilder";
+import { utilities } from "$/utilities";
+
 import { randomMaker } from "$/classes/RandomMaker";
 import { requesterCreator } from "$/classes/Requester";
 
-import { arrayOfRoutes, routes } from "@/http/routes";
+import { ClientSocket } from "@/types";
 
 import { errors } from "@/variables/errors";
 
+import { arrayOfRoutes, routes } from "@/websocket/events";
+
 describe("checkBodyFields middleware tests", () => {
-  let token;
-  beforeAll(async () => {
-    token = (await randomMaker.user()).token;
+  let socket: ClientSocket;
+
+  before(async () => {
+    socket = (await randomMaker.user()).socket;
   });
   //? Filter routes which has at least one input field
   const routesWithInputFields = arrayOfRoutes.filter(
@@ -19,19 +23,19 @@ describe("checkBodyFields middleware tests", () => {
   const routesWithoutAuth = routesWithInputFields.filter(
     (i) =>
       ![
-        routes.auth.signIn.fullUrl,
-        routes.auth.verify.fullUrl,
-        routes.auth.createNewUser.fullUrl,
-      ].includes(i.fullUrl)
+        routes.signIn.name,
+        routes.verify.name,
+        routes.createNewUser.name,
+      ].includes(i.name)
   );
 
   for (const route of routesWithoutAuth) {
-    const message = failTestBuilder
-      .create()
-      .createTestMessage(errors.INPUT_FIELDS_MISSING, route.fullUrl);
+    const message = utilities.createFailTestMessage(
+      errors.INPUT_FIELDS_MISSING,
+      route.name
+    );
     it(message, async () => {
-      await requesterCreator(token)
-        .create(route)
+      await requesterCreator(socket, route)
         .setError(errors.INPUT_FIELDS_MISSING)
         .setOptions({ shouldFilterRequestData: false })
         .sendFullFeaturedRequest();

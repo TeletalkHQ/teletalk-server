9fdd1d87c58583bf663b2971ef2c627f507d72cb
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 04:13:54 +0330

    random fix&cleanup

---
+++ __tests__/helpers/testHelperCollection.ts
@@ -0,0 +1,9 @@
+import { failCollection } from "$/helpers/fail";
+import { successCollection } from "$/helpers/success";
+
+const testHelperCollection = {
+  fail: failCollection,
+  success: successCollection,
+};
+
+export { testHelperCollection };

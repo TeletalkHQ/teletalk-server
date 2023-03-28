c565af67616a7e5686dd2451315e3dabab575f92
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 03:52:10 +0330

    refactor chats fail helper

---
+++ __tests__/helpers/fail/chats.ts
@@ -0,0 +1,8 @@
+import { FailTestExecutor } from "$/types";
+
+//TODO: Add chats fail tests
+const chatsFailTest: FailTestExecutor = () => {
+  logger.debug("Add chats fail tests");
+};
+
+export { chatsFailTest };

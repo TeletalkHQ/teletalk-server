f514c5bbaf6ae84331cbee82443fed66b8919202
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 03:56:32 +0330

    refactor countryName success helper

--- __tests__/helpers/success/countryName.ts
+++ __tests__/helpers/success/countryName.ts
@@ -1,21 +1,19 @@
 import { successTestBuilder } from "$/classes/SuccessTestBuilder";
-import { testVariablesManager } from "$/classes/TestVariablesManager";
 
 import { models } from "@/models";
 
+import { SuccessTestExecutor } from "$/types";
+
 const userModels = models.native.user;
 
-const countryNameSuccessTest = (
+const countryNameSuccessTest: SuccessTestExecutor = (
   { equalValue, testValue },
-  {
-    stringEquality = true,
-    modelCheck = true,
-  } = testVariablesManager.successTestDefaultOptions
+  options
 ) => {
   successTestBuilder
     .create()
     .setVariables(userModels.countryName, equalValue, testValue)
-    .setOptions({ modelCheck, stringEquality })
+    .setOptions(options)
     .addCommonTest()
     .emptyCheck()
     .run();

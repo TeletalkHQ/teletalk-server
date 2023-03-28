432d7ed8bf507e6fb41c56fd492ccb72fddb5ee6
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 03:45:52 +0330

    fix requesters

--- __tests__/utilities/other.ts
+++ __tests__/utilities/other.ts
@@ -1,14 +1,17 @@
+import { Socket } from "socket.io-client";
 import { randomMaker } from "utility-store";
-
-import { models } from "@/models";
 
 import { requesterCreator } from "$/classes/Requester";
 
-import { routes } from "@/http/routes";
+import { models } from "@/models";
+
+import { NativeError, SocketRoute } from "@/types";
 
 import { countries } from "@/variables/others/countries";
 
-const getWrongCountryCode = () => {
+import { routes } from "@/websocket/events";
+
+const getWrongCountryCode = (): string => {
   const randomCountryCode = randomMaker.stringNumber(
     models.native.user.countryCode.maxlength.value
   );
@@ -22,27 +25,42 @@ const getWrongCountryCode = () => {
   return randomCountryCode;
 };
 
-const makeRequester = (route) => (token) =>
-  requesterCreator(token).create(route);
+const makeRequester = (route: SocketRoute) => (socketClient: Socket) =>
+  requesterCreator(socketClient, route);
+
+const createFailTestMessage = (
+  error: NativeError,
+  route: SocketRoute | string
+) => {
+  const name = typeof route === "object" ? route.name : route;
+  return `expected error: [route:${name}] [key:${error.key}] [reason:${
+    error.reason
+  }] - [ok:${error ? true : false}] `;
+};
 
 const requesters = {
-  addBlock: makeRequester(routes.user.addBlock),
-  addContact: makeRequester(routes.user.addContact),
-  createNewUser: makeRequester(routes.auth.createNewUser),
-  editContact: makeRequester(routes.user.editContact),
-  getAllStuff: makeRequester(routes.stuff.getAllStuffs),
-  getContacts: makeRequester(routes.user.getContacts),
-  getCurrentUserData: makeRequester(routes.user.getCurrentUserData),
-  getPublicUserData: makeRequester(routes.user.getPublicUserData),
-  logout: makeRequester(routes.auth.logout),
-  removeBlock: makeRequester(routes.user.removeBlock),
-  removeContact: makeRequester(routes.user.removeContact),
-  signIn: makeRequester(routes.auth.signIn),
-  updatePublicUserData: makeRequester(routes.user.updatePublicUserData),
-  verify: makeRequester(routes.auth.verify),
+  addBlock: makeRequester(routes.addBlock),
+  addContact: makeRequester(routes.addContact),
+  addContactWithCellphone: makeRequester(routes.addContactWithCellphone),
+  createNewUser: makeRequester(routes.createNewUser),
+  editContact: makeRequester(routes.editContact),
+  getContacts: makeRequester(routes.getContacts),
+  getCurrentUserData: makeRequester(routes.getCurrentUserData),
+  getPublicUserData: makeRequester(routes.getPublicUserData),
+  getStuff: makeRequester(routes.getStuff),
+  logout: makeRequester(routes.logout),
+  removeBlock: makeRequester(routes.removeBlock),
+  removeContact: makeRequester(routes.removeContact),
+  sendPrivateMessage: makeRequester(routes.sendPrivateMessage),
+  signIn: makeRequester(routes.signIn),
+  updatePublicUserData: makeRequester(routes.updatePublicUserData),
+  verify: makeRequester(routes.verify),
+  getPrivateChats: makeRequester(routes.getPrivateChats),
+  getPrivateChat: makeRequester(routes.getPrivateChat),
 };
 
 const otherUtilities = {
+  createFailTestMessage,
   getWrongCountryCode,
   makeRequester,
   requesters,

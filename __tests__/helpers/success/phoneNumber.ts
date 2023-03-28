176e0888d33c44e60fa13ad3cfd8846d3478af32
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 04:08:21 +0330

    refactor phoneNumber success helper

--- __tests__/helpers/success/phoneNumber.ts
+++ __tests__/helpers/success/phoneNumber.ts
@@ -1,21 +1,19 @@
 import { successTestBuilder } from "$/classes/SuccessTestBuilder";
-import { testVariablesManager } from "$/classes/TestVariablesManager";
 
 import { models } from "@/models";
 
+import { SuccessTestExecutor } from "$/types";
+
 const userModels = models.native.user;
 
-const phoneNumberSuccessTest = (
+const phoneNumberSuccessTest: SuccessTestExecutor = (
   { equalValue, testValue },
-  {
-    stringEquality = true,
-    modelCheck = true,
-  } = testVariablesManager.successTestDefaultOptions
+  options
 ) => {
   successTestBuilder
     .create()
     .setVariables(userModels.phoneNumber, equalValue, testValue)
-    .setOptions({ modelCheck, stringEquality })
+    .setOptions(options)
     .emptyCheck()
     .numericCheck()
     .addCommonTest()

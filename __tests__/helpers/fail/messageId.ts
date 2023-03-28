e39f38bba9c3b99a84fba0cdab2e08a9571c4660
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 04:06:18 +0330

    refactor messageId fail helper

---
+++ __tests__/helpers/fail/messageId.ts
@@ -0,0 +1,8 @@
+import { FailTestExecutor } from "$/types";
+
+//TODO Add message id fail tests
+const messageIdFailTest: FailTestExecutor = () => {
+  logger.debug("Add message id fail tests");
+};
+
+export { messageIdFailTest };

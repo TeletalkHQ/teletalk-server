f46735ad54309b9bf6e44e4ee6615d7e0b353abe
s-stalwart-s <stalwart.programmer@gmail.com>
2023-03-28 04:08:06 +0330

    refactor phoneNumber fail helper

--- __tests__/helpers/fail/phoneNumber.ts
+++ __tests__/helpers/fail/phoneNumber.ts
@@ -2,15 +2,18 @@ import { failTestBuilder } from "$/classes/FailTestBuilder";
 
 import { models } from "@/models";
 
+import { FailTestExecutor } from "$/types";
+
 const userModels = models.native.user;
 
-const phoneNumberFailTest = (configuredRequester, data) => {
+const phoneNumberFailTest: FailTestExecutor = (configuredRequester, data) => {
   failTestBuilder
     .create(configuredRequester, data, userModels.phoneNumber, "phoneNumber")
     .missing()
     .overload()
     .invalidType()
-    .empty.numeric()
+    .empty()
+    .numeric()
     .minlength()
     .maxlength();
 };

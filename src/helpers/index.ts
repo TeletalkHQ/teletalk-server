7388df41409e3794d9c44241503dca60f22a5124
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-17 21:21:39 +0330

    add new helper: clientIdGenerator

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,7 +1,15 @@
+import { randomMaker } from "utility-store";
+
+import { envManager } from "@/classes/EnvironmentManager";
+
 import { databaseHelpers } from "@/helpers/database";
+
+const clientIdGenerator = () =>
+  randomMaker.id(envManager.getEnvironment().CLIENT_ID_LENGTH);
 
 const helpers = {
   ...databaseHelpers,
+  clientIdGenerator,
 };
 
 export { helpers };

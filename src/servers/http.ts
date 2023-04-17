0ea5f092ac55dc4d793d8248de1ed3bfdf3ea93e
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-17 21:22:13 +0330

    update crateHttpServer

--- src/servers/http.ts
+++ src/servers/http.ts
@@ -1,6 +1,31 @@
 import http from "http";
 
-const crateHttpServer = <T extends http.RequestListener>(app: T) =>
-  http.createServer(app);
+type ClientIdGenerator = () => string;
+
+const crateHttpServer = (
+  clientIdGenerator: ClientIdGenerator,
+  app?: http.RequestListener
+) => {
+  return http.createServer(app || defaultApp(clientIdGenerator));
+};
+
+const defaultApp = (clientIdGenerator: ClientIdGenerator) => {
+  return (
+    req: http.IncomingMessage,
+    res: http.ServerResponse<http.IncomingMessage>
+  ) => {
+    if (req.url === "/setClientId") {
+      const clientId = clientIdGenerator();
+      res.writeHead(200, {
+        "Set-Cookie": `clientId=${clientId}; HttpOnly; SameSite=Strict`,
+        "Content-Type": "text/plain",
+      });
+      res.end("Client ID has been set!");
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  };
+};
 
 export { crateHttpServer };

7c41b740fd07dcb5032cb23b75e58afe0cebedab
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-23 17:35:07 +0330

    add test services

--- testSrc/services/index.ts
+++ testSrc/services/index.ts
@@ -1,5 +1,6 @@
 import { userServices } from "$/services/user";
+import { privateChatServices } from "$/services/privateChat";
 
-const services = { ...userServices };
+const services = { ...userServices, ...privateChatServices };
 
 export { services };

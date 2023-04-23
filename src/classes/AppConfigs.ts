742f60c5930d83e91fd1c690ada29a5471bc63fb
s-stalwart-s <stalwart.programmer@gmail.com>
2023-04-23 17:43:55 +0330

    refactor app configuration

--- src/classes/AppConfigs.ts
+++ src/classes/AppConfigs.ts
@@ -1,45 +1,23 @@
+import path from "path";
+
+import dotenv from "dotenv";
+import { LoggerChalker } from "logger-chalker";
 import { Trier } from "simple-trier";
 
 import { envManager } from "@/classes/EnvironmentManager";
 
-const { PORT } = envManager.getEnvironment();
-// const RANDOM_PORT = randomMaker.numberWithRange(8000, 10000);
+import { EnvFileName, Environments } from "@/types";
 
 class AppConfigs {
-  private ENVIRONMENTS = envManager.getEnvironment();
-
-  private CONFIGS = {
-    MONGO_DB: {
-      MONGO_URL: this.ENVIRONMENTS.MONGO_URL,
-      MONGO_URL_FULL: makeMongoFullUrl(),
-    },
-    REDIS: {
-      REDIS_HOST: this.ENVIRONMENTS.REDIS_HOST,
-      REDIS_PASSWORD: this.ENVIRONMENTS.REDIS_PASSWORD,
-      REDIS_PORT: this.ENVIRONMENTS.REDIS_PORT,
-    },
-    APP: {
-      ENVIRONMENT: this.ENVIRONMENTS.NODE_ENV,
-      HOSTNAME: "localhost",
-      PORT,
-      SELF_EXEC: this.ENVIRONMENTS.SELF_EXEC,
-      SESSION_MAIN_SECRET: this.ENVIRONMENTS.SESSION_MAIN_SECRET,
-      SESSION_SIGN_IN_SECRET: this.ENVIRONMENTS.SESSION_SIGN_IN_SECRET,
-    },
-    SMS_CLIENT: {
-      SMS_PROVIDER_1_HOST: this.ENVIRONMENTS.SMS_PROVIDER_1_HOST,
-      SMS_PROVIDER_1_ROUTE: this.ENVIRONMENTS.SMS_PROVIDER_1_ROUTE,
-      SMS_PROVIDER_1_SENDER: this.ENVIRONMENTS.SMS_PROVIDER_1_SENDER,
-      SMS_PROVIDER_1_SESSION: this.ENVIRONMENTS.SMS_PROVIDER_1_SESSION,
-      SMS_PROVIDER_2_HOST: this.ENVIRONMENTS.SMS_PROVIDER_2_HOST,
-      SMS_PROVIDER_2_REPORT_URL: this.ENVIRONMENTS.SMS_PROVIDER_2_REPORT_URL,
-      SMS_PROVIDER_2_ROUTE: this.ENVIRONMENTS.SMS_PROVIDER_2_ROUTE,
-      SMS_PROVIDER_2_SESSION: this.ENVIRONMENTS.SMS_PROVIDER_2_SESSION,
-      SMS_PROVIDER_SELECTOR: this.ENVIRONMENTS.SMS_PROVIDER_SELECTOR,
-    },
-  };
+  private ENVIRONMENTS: Environments;
 
   async setup() {
+    this.registerCustomGlobals();
+    this.registerEnvironments("base");
+    this.registerEnvironments(envManager.getNodeEnv());
+
+    this.setEnvironments(envManager.getEnvironment());
+
     const { NODE_ENV, LOG_LEVEL } = this.ENVIRONMENTS;
     logger.setLevel(logger.levels[LOG_LEVEL]);
 
@@ -50,8 +28,51 @@ class AppConfigs {
       });
   }
 
+  private registerCustomGlobals() {
+    global.logger = new LoggerChalker();
+  }
+
+  private registerEnvironments(fileName: EnvFileName) {
+    dotenv.config({
+      path: path.join(process.cwd(), "environments", `${fileName}.env`),
+      override: true,
+    });
+  }
+
+  private setEnvironments(e: Environments) {
+    this.ENVIRONMENTS = e;
+  }
+
   getConfigs() {
-    return this.CONFIGS;
+    return {
+      APP: {
+        ENVIRONMENT: this.ENVIRONMENTS.NODE_ENV,
+        HOSTNAME: "localhost",
+        PORT: this.ENVIRONMENTS.PORT,
+        SELF_EXEC: this.ENVIRONMENTS.SELF_EXEC,
+        //TODO: Move to AUTH
+        SESSION_MAIN_SECRET: this.ENVIRONMENTS.SESSION_MAIN_SECRET,
+        SESSION_SIGN_IN_SECRET: this.ENVIRONMENTS.SESSION_SIGN_IN_SECRET,
+      },
+      DB: {
+        MONGO_URL: this.ENVIRONMENTS.MONGO_URL,
+        MONGO_URL_FULL: makeMongoFullUrl(),
+        REDIS_HOST: this.ENVIRONMENTS.REDIS_HOST,
+        REDIS_PASSWORD: this.ENVIRONMENTS.REDIS_PASSWORD,
+        REDIS_PORT: this.ENVIRONMENTS.REDIS_PORT,
+      },
+      SMS_CLIENT: {
+        SMS_PROVIDER_1_HOST: this.ENVIRONMENTS.SMS_PROVIDER_1_HOST,
+        SMS_PROVIDER_1_ROUTE: this.ENVIRONMENTS.SMS_PROVIDER_1_ROUTE,
+        SMS_PROVIDER_1_SENDER: this.ENVIRONMENTS.SMS_PROVIDER_1_SENDER,
+        SMS_PROVIDER_1_SESSION: this.ENVIRONMENTS.SMS_PROVIDER_1_SESSION,
+        SMS_PROVIDER_2_HOST: this.ENVIRONMENTS.SMS_PROVIDER_2_HOST,
+        SMS_PROVIDER_2_REPORT_URL: this.ENVIRONMENTS.SMS_PROVIDER_2_REPORT_URL,
+        SMS_PROVIDER_2_ROUTE: this.ENVIRONMENTS.SMS_PROVIDER_2_ROUTE,
+        SMS_PROVIDER_2_SESSION: this.ENVIRONMENTS.SMS_PROVIDER_2_SESSION,
+        SMS_PROVIDER_SELECTOR: this.ENVIRONMENTS.SMS_PROVIDER_SELECTOR,
+      },
+    };
   }
 }
 

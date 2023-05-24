1816ee3e84a432b04bae39d0fa9ed893f856abf3
s-stalwart-s <stalwart.programmer@gmail.com>
2023-05-25 00:25:54 +0330

    feat: implement verifyClient middleware tests

---
+++ testSrc/__tests__/e2e/middleware/verifyClient.spec.ts
@@ -0,0 +1,34 @@
+import { randomMaker } from "utility-store";
+
+import { clientInitializer } from "$/classes/ClientInitializer";
+import { requesterMaker } from "$/classes/Requester";
+
+import { helpers } from "$/helpers";
+
+import { models } from "@/models";
+
+import { errors } from "@/variables";
+
+import { arrayOfRoutes } from "@/websocket/events";
+
+//REFACTOR: fail suit message like this
+describe("verifyClient fail tests", () => {
+  for (const route of arrayOfRoutes) {
+    const title = helpers.createFailTestMessage(
+      errors.client_invalid,
+      route.name
+    );
+    it(title, async () => {
+      const ci = clientInitializer();
+      ci.setClient(randomMaker.string(models.native.clientId.maxLength));
+      ci.makeClientCookie();
+      ci.initClient();
+      ci.connect();
+
+      await requesterMaker(ci.getClient(), route).sendFullFeaturedRequest(
+        {},
+        errors.client_invalid
+      );
+    });
+  }
+});

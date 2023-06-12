b2b83efaedf1ef961554b59623d0d2d593905fc6
s-stalwart-s <stalwart.programmer@gmail.com>
2023-06-12 12:44:16 +0330

    fix: cors issue in http default app

--- src/http/index.ts
+++ src/http/index.ts
@@ -2,6 +2,7 @@ import http from "http";
 
 import cors from "cors";
 import express from "express";
+
 import { clientManager } from "@/classes/ClientIdManager";
 
 const expressApp = express();
@@ -31,16 +32,28 @@ const defaultApp = () => {
     req: http.IncomingMessage,
     res: http.ServerResponse<http.IncomingMessage>
   ) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Request-Method", "*");
+    res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+
     if (req.url === "/setClientId") {
       const clientStr = await clientManager.signClient();
-      res.writeHead(200, {
-        "Set-Cookie": `client=${clientStr}; HttpOnly; SameSite=Strict`,
-        "Content-Type": "text/plain",
-      });
+      res.setHeader("Content-Type", "text/plain");
+      res.setHeader(
+        "Set-Cookie",
+        `client=${clientStr}; HttpOnly; SameSite=Strict`
+      );
+      res.statusCode = 200;
+
       res.end("Client has been set!");
     } else {
       res.writeHead(404);
       res.end();
     }
   };
+};
+
+export const crateHttpServer = (app?: http.RequestListener) => {
+  return http.createServer(app || expressApp || defaultApp());
 };

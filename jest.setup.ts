d422bbf6ee0717500283d902a134d68bbcebf010
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-14 01:22:11 +0330

    feat: refactor jest configs

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,8 +4,6 @@ import { randomMaker } from "utility-store";
 import { appConfigs } from "~/classes/AppConfigs";
 import { runner } from "~/index";
 
-// import { services } from "$/services/index";
-
 await (async () => {
   appConfigs.setPort(randomMaker.numberWithRange(8000, 50000));
 
@@ -15,10 +13,4 @@ await (async () => {
   });
 
   await runner();
-  // await resetDatabase();
 })();
-
-// const resetDatabase = async () => {
-//   await services.deleteAllUsers();
-//   await services.deleteAllPrivateChats();
-// };

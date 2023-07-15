80484177dafa070e2bb0741a81346be9405805bc
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-15 19:47:28 +0330

    feat: improve jest configs

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,11 +1,19 @@
+import { jest } from "@jest/globals";
 import { Trier } from "simple-trier";
 import { randomMaker } from "utility-store";
 
 import { appConfigs } from "~/classes/AppConfigs";
 import { runner } from "~/index";
 
+jest.retryTimes(10, {
+  logErrorsBeforeRetry: false,
+});
+
 await (async () => {
   appConfigs.setPort(randomMaker.numberWithRange(8000, 50000));
+
+  logger.offAll();
+  logger.on("error");
 
   Trier.changeGlobalConfigs({
     callerName: "unknownCaller",

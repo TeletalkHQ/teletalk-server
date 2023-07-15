0d1adaf690018f9331a0c9af71055de7f3377eaf
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-15 20:06:28 +0330

    fix: middleware tests initializers

--- testSrc/__tests__/e2e/middleware/checkCurrentUser.spec.ts
+++ testSrc/__tests__/e2e/middleware/checkCurrentUser.spec.ts
@@ -6,36 +6,36 @@ import { eventsWithAuth } from "~/websocket/events";
 import { randomMaker } from "@/classes/RandomMaker";
 import { utils } from "@/utils";
 
+const filteredEvents = eventsWithAuth.filter(
+  (i) => i.name !== "verify" && i.name !== "createNewUser"
+);
+
 describe("checkCurrentUser middleware fail tests", () => {
-  eventsWithAuth
-    .filter((i) => i.name !== "verify" && i.name !== "createNewUser")
-    .forEach((event) => {
-      it(
-        utils.createFailTestMessage(
-          errorStore.find("CURRENT_USER_NOT_EXIST"),
-          event.name
-        ),
-        async () => {
-          const wrongUserId = randomMaker.string(
-            models.native.userId.maxLength
-          );
+  for (const event of filteredEvents) {
+    it(
+      utils.createFailTestMessage(
+        errorStore.find("CURRENT_USER_NOT_EXIST"),
+        event.name
+      ),
+      async () => {
+        const wrongUserId = randomMaker.string(models.native.userId.maxLength);
 
-          const { socket } = await randomMaker.user();
+        const { socket } = await randomMaker.user();
 
-          const client = (await clientStore.find(socket.clientId))!;
-          await clientStore.update(socket.clientId, {
-            ...client,
-            userId: wrongUserId,
-          });
+        const client = (await clientStore.find(socket.clientId))!;
+        await clientStore.update(socket.clientId, {
+          ...client,
+          userId: wrongUserId,
+        });
 
-          const data = utils.generateDynamicData(event.inputFields);
-          await utils.requesterCollection[event.name](
-            socket
-          ).sendFullFeaturedRequest(
-            data as any,
-            errorStore.find("CURRENT_USER_NOT_EXIST")
-          );
-        }
-      );
-    });
+        const data = utils.generateDynamicData(event.inputFields);
+        await utils.requesterCollection[event.name](
+          socket
+        ).sendFullFeaturedRequest(
+          data as any,
+          errorStore.find("CURRENT_USER_NOT_EXIST")
+        );
+      }
+    );
+  }
 });

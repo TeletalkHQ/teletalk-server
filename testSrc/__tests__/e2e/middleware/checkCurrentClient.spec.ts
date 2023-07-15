d976c5d4fc52f4b052c233bdbfca3ae9dc1e9899
s-stalwart-s <stalwart.programmer@gmail.com>
2023-07-15 20:06:28 +0330

    fix: middleware tests initializers

--- testSrc/__tests__/e2e/middleware/checkCurrentClient.spec.ts
+++ testSrc/__tests__/e2e/middleware/checkCurrentClient.spec.ts
@@ -8,38 +8,40 @@ import { randomMaker } from "@/classes/RandomMaker";
 import { RequesterCollection } from "@/types";
 import { utils } from "@/utils";
 
+const filteredEvents = eventsWithAuth.filter(
+  (i) => i.name !== "verify" && i.name !== "createNewUser"
+);
+
 describe("checkCurrentUser middleware fail tests", () => {
-  eventsWithAuth
-    .filter((i) => i.name !== "verify" && i.name !== "createNewUser")
-    .forEach((event) => {
-      it(
-        utils.createFailTestMessage(
-          errorStore.find("CURRENT_CLIENT_NOT_EXIST"),
-          event.name
-        ),
-        async () => {
-          const { socket } = await randomMaker.user();
-          const newAuthClient = await clientManager.signClient();
-          const {
-            payload: { clientId: newClientId },
-          } = await clientManager.verifyClient(newAuthClient);
-          const ci = clientInitializer();
-          ci.setClient(newAuthClient).makeClientCookie().initClient().connect();
+  for (const event of filteredEvents) {
+    it(
+      utils.createFailTestMessage(
+        errorStore.find("CURRENT_CLIENT_NOT_EXIST"),
+        event.name
+      ),
+      async () => {
+        const { socket } = await randomMaker.user();
+        const newAuthClient = await clientManager.signClient();
+        const {
+          payload: { clientId: newClientId },
+        } = await clientManager.verifyClient(newAuthClient);
+        const ci = clientInitializer();
+        ci.setClient(newAuthClient).makeClientCookie().initClient().connect();
 
-          const client = (await clientStore.find(socket.clientId))!;
-          await clientStore.add(newClientId, {
-            ...client,
-            userId: client.userId,
-          });
+        const client = (await clientStore.find(socket.clientId))!;
+        await clientStore.add(newClientId, {
+          ...client,
+          userId: client.userId,
+        });
 
-          const data = utils.generateDynamicData(event.inputFields);
-          await utils.requesterCollection[
-            event.name as keyof RequesterCollection
-          ](ci.getClient()).sendFullFeaturedRequest(
-            data as any,
-            errorStore.find("CURRENT_CLIENT_NOT_EXIST")
-          );
-        }
-      );
-    });
+        const data = utils.generateDynamicData(event.inputFields);
+        await utils.requesterCollection[
+          event.name as keyof RequesterCollection
+        ](ci.getClient()).sendFullFeaturedRequest(
+          data as any,
+          errorStore.find("CURRENT_CLIENT_NOT_EXIST")
+        );
+      }
+    );
+  }
 });

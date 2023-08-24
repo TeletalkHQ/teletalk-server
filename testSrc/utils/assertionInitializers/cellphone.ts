3eb4c7386044ce87f3a87bb282789c8d8406ceb4
S-STALWART-S <stalwart.programmer@gmail.com>
2023-08-25 02:12:23 +0330

    fix: `cellphoneAssertionInitializer`

--- testSrc/utils/assertionInitializers/cellphone.ts
+++ testSrc/utils/assertionInitializers/cellphone.ts
@@ -5,20 +5,29 @@ import { countryCodeAssertionInitializer } from "@/utils/assertionInitializers/c
 import { countryNameAssertionInitializer } from "@/utils/assertionInitializers/countryName";
 import { phoneNumberAssertionInitializer } from "@/utils/assertionInitializers/phoneNumber";
 
-export const cellphoneAssertionInitializer: AssertionInitializer<Cellphone> = ({
-	equalValue,
-	testValue,
-}) => {
-	countryCodeAssertionInitializer({
-		equalValue: equalValue?.countryCode,
-		testValue: testValue.countryCode,
-	});
-	countryNameAssertionInitializer({
-		equalValue: equalValue?.countryName,
-		testValue: testValue.countryName,
-	});
-	phoneNumberAssertionInitializer({
-		equalValue: equalValue?.phoneNumber,
-		testValue: testValue.phoneNumber,
-	});
+export const cellphoneAssertionInitializer: AssertionInitializer<Cellphone> = (
+	{ equalValue, testValue },
+	options
+) => {
+	countryCodeAssertionInitializer(
+		{
+			equalValue: equalValue?.countryCode,
+			testValue: testValue.countryCode,
+		},
+		options
+	);
+	countryNameAssertionInitializer(
+		{
+			equalValue: equalValue?.countryName,
+			testValue: testValue.countryName,
+		},
+		options
+	);
+	phoneNumberAssertionInitializer(
+		{
+			equalValue: equalValue?.phoneNumber,
+			testValue: testValue.phoneNumber,
+		},
+		options
+	);
 };

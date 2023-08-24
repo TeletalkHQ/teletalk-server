b430c244b59ead8234cf614124d0dee26127fc4c
S-STALWART-S <stalwart.programmer@gmail.com>
2023-08-25 02:12:10 +0330

    fix: `contactsAssertionInitializer`

--- testSrc/utils/assertionInitializers/contacts.ts
+++ testSrc/utils/assertionInitializers/contacts.ts
@@ -12,13 +12,16 @@ export const contactsAssertionInitializer: AssertionInitializer<Contacts> = (
 	chai.expect(testValue).to.be.an(FIELD_TYPE.ARRAY);
 
 	testValue.forEach((item) => {
-		oneContactAssertionInitializer({
-			testValue: item,
-		});
-	}, options);
+		oneContactAssertionInitializer(
+			{
+				testValue: item,
+			},
+			{ ...options, stringEquality: false }
+		);
+	});
 
 	if (options?.stringEquality) {
-		chai.expect(testValue).to.be.an(FIELD_TYPE.ARRAY);
+		chai.expect(equalValue).to.be.an(FIELD_TYPE.ARRAY);
 		chai.expect(testValue.length).to.be.equal(equalValue!.length);
 
 		equalValue!.forEach((item) => {
@@ -26,10 +29,13 @@ export const contactsAssertionInitializer: AssertionInitializer<Contacts> = (
 
 			chai.expect(foundContact).to.be.an(FIELD_TYPE.OBJECT);
 
-			oneContactAssertionInitializer({
-				equalValue: item,
-				testValue: foundContact!,
-			});
-		}, options);
+			oneContactAssertionInitializer(
+				{
+					equalValue: item,
+					testValue: foundContact!,
+				},
+				options
+			);
+		});
 	}
 };
